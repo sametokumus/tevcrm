eacbff91bcca7666ab91b84b9e4a8acd724e05c2
codbt <info@codbt.com>
2022-04-21 16:02:37 +0300

    unique admin tablosu

--- database/migrations/2022_04_20_141947_create_admins_table.php
+++ database/migrations/2022_04_20_141947_create_admins_table.php
@@ -15,12 +15,12 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('name');
             $table->string('surname');
             $table->string('phone_number');
             $table->string('password');
-            $table->string('token')->nullable();
+            $table->string('token')->nullable()->unique();
             $table->tinyInteger('active')->default(1);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
