37a5433e69bc1d94e9caddf5a02eceafd100e86b
Samet <samet.okumus@codbt.com>
2022-05-22 23:37:06 +0300

    productruleweight

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -126,12 +126,12 @@ class ProductController extends Controller
                 ->leftJoin('product_types','product_types.id','=','products.type_id')
                 ->leftJoin('product_variation_groups','product_variation_groups.product_id','=','products.id')
                 ->select(DB::raw('(select id from product_variation_groups where product_id = products.id order by id asc limit 1) as variation_group'))
-//                ->leftJoin('product_variations','product_variations.variation_group_id','=','product_variation_groups.id')
+                ->leftJoin('product_variations','product_variations.variation_group_id','=','product_variation_groups.id')
 //                ->select(DB::raw('(select id from product_variations where variation_group_id = products.variation_group order by id asc limit 1) as variation_id'))
 //                ->select(DB::raw('(select image from product_images where variation_id = product_variations.id order by id asc limit 1) as image'))
 //                ->leftJoin('product_rules','product_rules.variation_id','=','product_variations.id')
 //                ->selectRaw('products.* ,brands.name as brand_name,product_types.name as type_name, product_rules.*')
-                ->selectRaw('products.* ,brands.name as brand_name,product_types.name as type_name')
+                ->selectRaw('products.* ,brands.name as brand_name,product_types.name as type_name, product_variations.id as xxx')
                 ->where('products.active',1)
                 ->get();
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['products' => $products]]);
