48d7fff77e59fc544fb1aca76e118d7656d14ba6
codbt <info@codbt.com>
2022-05-26 13:23:13 +0300

    fıtı fıtı

--- database/migrations/2022_05_26_083944_create_delivery_prices_table.php
+++ database/migrations/2022_05_26_083944_create_delivery_prices_table.php
@@ -16,6 +16,7 @@ class CreateDeliveryPricesTable extends Migration
         Schema::create('delivery_prices', function (Blueprint $table) {
             $table->id();
             $table->decimal('min_value');
+            $table->decimal('test');
             $table->decimal('max_value');
             $table->decimal('price');
             $table->tinyInteger('active')->default(1);
