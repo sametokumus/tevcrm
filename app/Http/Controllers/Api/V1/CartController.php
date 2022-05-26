e68619b695214b227a5305c4a6b46338e28bc7d6
Samet <samet.okumus@codbt.com>
2022-05-26 11:53:07 +0300

    productruleweight

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\CartDetail;
+use App\Models\DeliveryPrice;
 use App\Models\Product;
 use App\Models\ProductImage;
 use App\Models\ProductRule;
@@ -139,7 +140,7 @@ class CartController extends Controller
             $cart_details = CartDetail::query()->where('cart_id',$cart->cart_id)->where('active',1)->get();
             $cart_price = 0;
             $cart_tax = 0;
-            $cart_delivery_price = 0;
+            $weight = 0;
             foreach ($cart_details as $cart_detail){
                 $product = Product::query()->where('id',$cart_detail->product_id)->first();
                 $variation = ProductVariation::query()->where('id',$cart_detail->variation_id)->first();
@@ -153,26 +154,25 @@ class CartController extends Controller
                 if ($rule->discounted_price == null){
                     $cart_detail_price = $rule->regular_price * $cart_detail->quantity;
                     $cart_detail_tax = $rule->regular_tax * $cart_detail->quantity;
-                    $cart_detail_delivery_price = $product->delivery_price * $cart_detail->quantity;
                 }else{
                     $cart_detail_price = $rule->discounted_price * $cart_detail->quantity;
                     $cart_detail_tax = $rule->discounted_tax * $cart_detail->quantity;
-                    $cart_detail_delivery_price = $product->delivery_price * $cart_detail->quantity;
                 }
-                if($product->is_free_shipping == 1){
-                    $cart_detail_delivery_price = 0.00;
-                }
+                $weight = $weight + $rule->weight;
+//                if($product->is_free_shipping == 1){
+//                    $cart_detail_delivery_price = 0.00;
+//                }
                 $cart_detail['sub_total_price'] = $cart_detail_price;
                 $cart_detail['sub_total_tax'] = $cart_detail_tax;
-                $cart_detail['sub_total_delivery'] = $cart_detail_delivery_price;
                 $cart_price += $cart_detail_price;
                 $cart_tax += $cart_detail_tax;
-                $cart_delivery_price += $cart_detail_delivery_price;
 
             }
             $cart['cart_details'] = $cart_details;
             $cart['total_price'] = $cart_price;
             $cart['total_tax'] = $cart_tax;
+
+            $cart_delivery_price = DeliveryPrice::query()->where('min_value', '>=', $weight)->where('max_value', '<', $weight)->first()->price;
             $cart['total_delivery'] = $cart_delivery_price;
 
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['cart' => $cart]]);
@@ -200,7 +200,7 @@ class CartController extends Controller
             Cart::query()->where('cart_id', $cart_id)->update([
                 'active' => 0
             ]);
-            
+
             return response(['message' => 'Sepet silme işlemi başarılı.','status' => 'success']);
         } catch (ValidationException $validationException) {
             return response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.', 'status' => 'validation-001']);
