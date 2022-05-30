53ebbb61b7bd4815a6c8cbb5ef715b7a7eb3f015
codbt <info@codbt.com>
2022-05-30 15:22:39 +0300

    fıtı fıtı

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\Cart;
+use App\Models\CartDetail;
+use App\Models\City;
+use App\Models\Country;
 use App\Models\Order;
 use App\Models\OrderProduct;
 use App\Models\OrderStatus;
 use App\Models\OrderStatusHistory;
+use App\Models\Product;
+use App\Models\ProductRule;
+use App\Models\ProductVariation;
 use Faker\Provider\Uuid;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
@@ -22,6 +29,19 @@ class OrderController extends Controller
         try {
             $order_status = OrderStatus::query()->where('is_default',1)->first();
             $order_quid = Uuid::uuid();
+            $shipping_id = $request->shipping_address_id;
+            $billing_id = $request->billing_address_id;
+            $shipping = Address::query()->where('id',$shipping_id)->first();
+            $country = Country::query()->where('id',$shipping->country_id)->first();
+            $city = City::query()->where('id',$shipping->city_id)->first();
+            $shipping_address = $shipping->name. " - " .$shipping->surname. " - " .$shipping->address_1. " - " .$shipping->address_2. " - " .$shipping->postal_code. " - " .$shipping->phone. " - ". $country->name. " - " .$city->name;
+
+
+            $billing = Address::query()->where('id',$billing_id)->first();
+            $billing_country = Country::query()->where('id',$billing->country_id)->first();
+            $billing_city = City::query()->where('id',$billing->city_id)->first();
+            $billing_address = $billing->name. "-" .$billing->surname. " - " .$billing->address_1. " - " .$billing->address_2. " - " .$billing->postal_code. " - " .$billing->phone. " - ". $billing_country->name. " - " .$billing_city->name;
+
                 $order_id = Order::query()->insertGetId([
                     'order_id' => $order_quid,
                     'user_id' => $request->user_id,
@@ -30,8 +50,8 @@ class OrderController extends Controller
                     'status_id' => $order_status->id,
                     'shipping_address_id' => $request->shipping_address_id,
                     'billing_address_id' => $request->billing_address_id,
-                    'shipping_address' => $request->shipping_address,
-                    'billing_address' => $request->billing_address,
+                    'shipping_address' => $shipping_address,
+                    'billing_address' => $billing_address,
                     'comment' => $request->comment,
                     'total_discount' => $request->total_discount,
                     'total_discount_tax' => $request->total_discount_tax,
@@ -42,22 +62,24 @@ class OrderController extends Controller
                     'is_partial' => $request->is_partial,
                     'is_paid' => $request->is_paid
                 ]);
-                $order_cart_id = Order::query()->where('id',$order_id)->first()->cart_id;
-                $user_id = Order::query()->where('id',$order_id)->first()->user_id;
-                Cart::query()->where('cart_id',$order_cart_id)->update([
-                    'user_id' => $user_id
+
+                Cart::query()->where('cart_id',$request->cart_id)->update([
+                    'user_id' => $request->user_id
                 ]);
 
-            $order_products = json_decode(json_encode($request->order_products));
-            foreach ($order_products as $order_product){
+                $carts = CartDetail::query()->where('cart_id',$request->cart_id)->get();
+            foreach ($carts as $cart){
+                $product = Product::query()->where('id',$cart->product_id)->first();
+                $variation = ProductVariation::query()->where('id',$cart->variation_id)->first();
+                $rule = ProductRule::query()->where('variation_id',$variation->id)->first();
                 OrderProduct::query()->insert([
                     'order_id' => $order_quid,
-                    'product_id' => $order_product->product_id,
-                    'name' => $order_product->name,
-                    'sku' => $order_product->sku,
-                    'price' => $order_product->price,
-                    'tax' => $order_product->tax,
-                    'quantity' => $order_product->quantity
+                    'product_id' => $product,
+                    'name' => $product->name,
+                    'sku' => $variation->sku,
+                    'price' => $cart->price,
+                    'tax' => '',
+                    'quantity' => $cart->quantity
                 ]);
             }
 
