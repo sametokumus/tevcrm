0013127f5da10b3182693dff95e8434f9f1b1d83
codbt <info@codbt.com>
2022-06-03 01:36:54 +0300

    fıtı fıtı

--- app/Http/Controllers/Api/V1/CreditCardController.php
+++ app/Http/Controllers/Api/V1/CreditCardController.php
@@ -12,12 +12,12 @@ class CreditCardController extends Controller
 {
     public function getCreditCarts(){
         try {
-            $credit_cart_installments = CreditCardInstallment::query()->where('active',1)->get();
-            foreach ($credit_cart_installments as $credit_cart_installment){
-                $credit_cart = CreditCard::query()->where('id',$credit_cart_installment->credit_cart_id)->first();
-                $credit_cart_installment['installment'] = $credit_cart;
+            $credit_cards = CreditCard::query()->where('active',1)->get();
+            foreach ($credit_cards as $credit_card){
+                $credit_card_installment = CreditCardInstallment::query()->where('credit_card_id',$credit_card->id)->get();
+                $credit_card['installment'] = $credit_card_installment;
             }
-            return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['credit_cart_installments' => $credit_cart_installments]]);
+            return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['credit_card' => $credit_card]]);
         } catch (QueryException $queryException) {
             return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001','a' => $queryException->getMessage()]);
         }
