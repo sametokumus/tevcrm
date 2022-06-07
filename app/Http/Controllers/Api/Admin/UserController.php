aa8117894d906dc916f386fcae58873752e59d37
codbt <info@codbt.com>
2022-06-07 12:19:44 +0300

    fıtı fıtı

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
@@ -11,10 +12,13 @@ class UserController extends Controller
 {
     public function getUsers(){
         try {
-            $users = User::query()->where('active',1)->where('verified',1)->get();
+            $users = User::query()
+                ->leftJoin('user_profiles','user_profiles.user_id','=','users.id')
+                ->selectRaw('users.*, user_profiles.name, user_profiles.surname')
+                ->get();
             return response(['message' => 'İşlem başarılı.','status' => 'success','object' => ['users' => $users]]);
         } catch (QueryException $queryException){
-            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001','err' => $queryException->getMessage()]);
         }
     }
 }
