6dba1897733405a677095feae91651e83b04e4b1
Samet <samet.okumus@codbt.com>
2022-07-21 01:33:03 +0300

    slider db

---
+++ database/migrations/2022_07_20_222702_create_sliders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->text('bg_url');
+            $table->text('image_url');
+            $table->text('content');
+            $table->integer('order')->default(1);
+            $table->tinyInteger('active')->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
