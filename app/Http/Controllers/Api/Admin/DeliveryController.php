8c52d8e64833d8b8c8f7315c0373892797ceaa21
Samet <samet.okumus@codbt.com>
2022-07-27 22:41:02 +0300

    regional delivery 1

---
+++ app/Http/Controllers/Api/Admin/DeliveryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\City;
+use App\Models\DeliveryPrice;
+use App\Models\RegionalDeliveryPrice;
+use Illuminate\Http\Request;
+
+class DeliveryController extends Controller
+{
+    public function syncCitiesToRegionalDelivery()
+    {
+        try {
+            $cities = City::query()->where('active',1)->get();
+            foreach ($cities as $city){
+                $delivery_prices = DeliveryPrice::query()->where('active', 1)->get();
+                foreach ($delivery_prices as $delivery_price){
+                    $check_regional_delivery = RegionalDeliveryPrice::query()->where('city_id', $city->id)->where('delivery_price_id', $delivery_price->id)->where('active', 1)->count();
+                    if($check_regional_delivery == 0){
+                        RegionalDeliveryPrice::query()->insert([
+                            'city_id' => $city->id,
+                            'delivery_price_id' => $delivery_price->id,
+                            'price' => $delivery_price->price
+                        ]);
+                    }
+                }
+            }
+            return response(['message' => 'İşlem Başarılı.', 'status' => 'success']);
+        } catch (QueryException $queryException) {
+            return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001']);
+        }
+    }
+}
--- routes/api_admin.php
+++ routes/api_admin.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\Admin\SliderController;
 use App\Http\Controllers\Api\Admin\SeoController;
 use App\Http\Controllers\Api\Admin\CouponController;
+use App\Http\Controllers\Api\Admin\DeliveryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -214,6 +215,10 @@ Route::middleware(['auth:sanctum', 'type.admin'])->group(function (){
     Route::post('coupon/addCoupon', [CouponController::class, 'addCoupon']);
     Route::post('coupon/updateCoupon/{id}', [CouponController::class, 'updateCoupon']);
     Route::get('coupon/getCoupons', [CouponController::class, 'getCoupons']);
+
+
+    Route::get('delivery/syncCitiesToRegionalDelivery', [DeliveryController::class, 'syncCitiesToRegionalDelivery']);
+
 
 
 });
