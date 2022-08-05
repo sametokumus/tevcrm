853a5f9848495785f6086e749e36a7823ba577e2
Samet <samet.okumus@codbt.com>
2022-08-05 08:31:25 +0300

    popup 1

---
+++ app/Http/Controllers/Api/V1/PopupController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Popup;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class PopupController extends Controller
+{
+    public function getPopups()
+    {
+        try {
+            $popups = Popup::query()->where('active',1)->get();
+            return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['popups' => $popups]]);
+        } catch (QueryException $queryException) {
+            return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001']);
+        }
+    }
+
+    public function getPopupById($popup_id){
+        try {
+            $popup = Popup::query()->where('id',$popup_id)->first();
+            return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['popup' => $popup]]);
+        } catch (QueryException $queryException) {
+            return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001']);
+        }
+    }
+}
