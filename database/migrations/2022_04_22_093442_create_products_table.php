ab95fdafbac37ae3829c35c930134d78692328e6
Samet <samet.okumus@codbt.com>
2022-08-31 17:09:19 +0300

    getProduct

--- database/migrations/2022_04_22_093442_create_products_table.php
+++ database/migrations/2022_04_22_093442_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('brand_id');
+            $table->bigInteger('type_id');
             $table->text('name');
             $table->text('description')->nullable();
             $table->text('short_description')->nullable();
