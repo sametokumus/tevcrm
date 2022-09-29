38b9085e7b9cec268bf4c003b556a61035ab995f
Samet <samet.okumus@codbt.com>
2022-09-29 14:06:09 +0300

    product-desc

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -231,7 +231,7 @@ class ProductController extends Controller
                 ->where('products.active', 1)
                 ->get();
 
-            $xx = TextContent::query()->where('id', 37)->first();
+            $xx = TextContent::query()->where('id', '37')->first();
 
 //            foreach ($products as $product){
 //                $product['name'] = TextContent::query()->where('id', '=', $product['name'])->first()->original_text;
