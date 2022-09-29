c0c2cea8d5fac1fc2541f02607eefd5216832de8
Samet <samet.okumus@codbt.com>
2022-09-29 14:14:57 +0300

    product-desc

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -231,15 +231,15 @@ class ProductController extends Controller
                 ->where('products.active', 1)
                 ->get();
 
-            $xx = TextContent::query()->where('id', $products[0]['name'])->first();
+//            foreach ($products as $product){
+//                $x1= TextContent::query()->where('id', $product['name'])->first();
+//                $product['name'] = $x1->original_text;
+//                $product['description'] = TextContent::query()->where('id', '=', $product['description'])->first()->original_text;
+//                $product['short_description'] = TextContent::query()->where('id', '=', $product['short_description'])->first()->original_text;
+//                $product['notes'] = TextContent::query()->where('id', '=', $product['notes'])->first()->original_text;
+//            }
 
-            foreach ($products as $product){
-                $x1= TextContent::query()->where('id', $product['name'])->first();
-                $product['name'] = $x1->original_text;
-                $product['description'] = TextContent::query()->where('id', '=', $product['description'])->first()->original_text;
-                $product['short_description'] = TextContent::query()->where('id', '=', $product['short_description'])->first()->original_text;
-                $product['notes'] = TextContent::query()->where('id', '=', $product['notes'])->first()->original_text;
-            }
+            $xx = TextContent::query()->where('id', $products[0]['name'])->first();
 
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['products' => $xx->original_text]]);
         } catch (QueryException $queryException) {
