9e0fd6b880fc33bc5d6339f804cc0707ecf14206
Samet <samet.okumus@codbt.com>
2022-09-29 14:09:50 +0300

    product-desc

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -234,7 +234,7 @@ class ProductController extends Controller
             $xx = TextContent::query()->where('id', $products[0]['name'])->first();
 
             foreach ($products as $product){
-                $product['name'] = TextContent::query()->where('id', '=', $product['name'])->first()->original_text;
+                $product['name'] = TextContent::query()->where('id', $product['name'])->first()->original_text;
                 $product['description'] = TextContent::query()->where('id', '=', $product['description'])->first()->original_text;
                 $product['short_description'] = TextContent::query()->where('id', '=', $product['short_description'])->first()->original_text;
                 $product['notes'] = TextContent::query()->where('id', '=', $product['notes'])->first()->original_text;
