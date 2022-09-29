454e5aeba58083e8a93ba405cb66d51784a0100f
Samet <samet.okumus@codbt.com>
2022-09-29 14:32:58 +0300

    product-desc

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -233,16 +233,14 @@ class ProductController extends Controller
 
             foreach ($products as $product){
                 $product_name = TextContent::query()->where('id',$product->name)->first();
-                $product['name'] = $product_name->original_text;
+                $dc = json_decode($product_name);
+                $obj = $dc[0];
+                $product['name_text'] = $obj->original_text;
+//                $product['name'] = TextContent::query()->where('id', $product->name)->first()->original_text;
+                $product['description'] = TextContent::query()->where('id', '=', $product['description'])->first()->original_text;
+                $product['short_description'] = TextContent::query()->where('id', '=', $product['short_description'])->first()->original_text;
+                $product['notes'] = TextContent::query()->where('id', '=', $product['notes'])->first()->original_text;
             }
-//            foreach ($products as $product){
-//                $product_name = TextContent::query()->where('id',$product->name)->first();
-//                $product['name_text'] = $product_name[0]['original_text'];
-////                $product['name'] = TextContent::query()->where('id', $product->name)->first()->original_text;
-//                $product['description'] = TextContent::query()->where('id', '=', $product['description'])->first()->original_text;
-//                $product['short_description'] = TextContent::query()->where('id', '=', $product['short_description'])->first()->original_text;
-//                $product['notes'] = TextContent::query()->where('id', '=', $product['notes'])->first()->original_text;
-//            }
 
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['products' => $products]]);
         } catch (QueryException $queryException) {
