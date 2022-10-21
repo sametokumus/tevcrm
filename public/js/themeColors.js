2117b5439239659b890f2e128b7c784d144b36f7
Samet <samet.okumus@codbt.com>
2022-10-22 01:47:08 +0300

    admins and roles

--- public/js/themeColors.js
+++ public/js/themeColors.js
@@ -406,13 +406,13 @@ function bgImage(e) {
 }
 
 // to check the value is hexa or not
-const isValidHex = (hexValue) => /^#([A-Fa-f0-9]{3,4}){1,2}$/.test(hexValue)
+isValidHex = (hexValue) => /^#([A-Fa-f0-9]{3,4}){1,2}$/.test(hexValue)
 
-const getChunksFromString = (st, chunkSize) => st.match(new RegExp(`.{${chunkSize}}`, "g"))
+getChunksFromString = (st, chunkSize) => st.match(new RegExp(`.{${chunkSize}}`, "g"))
     // convert hex value to 256
-const convertHexUnitTo256 = (hexStr) => parseInt(hexStr.repeat(2 / hexStr.length), 16)
+convertHexUnitTo256 = (hexStr) => parseInt(hexStr.repeat(2 / hexStr.length), 16)
     // get alpha value is equla to 1 if there was no value is asigned to alpha in function
-const getAlphafloat = (a, alpha) => {
+getAlphafloat = (a, alpha) => {
         if (typeof a !== "undefined") { return a / 255 }
         if ((typeof alpha != "number") || alpha < 0 || alpha > 1) {
             return 1
