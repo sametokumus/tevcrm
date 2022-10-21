ece5c43cf4978199cef47cf2f6329d8348a05ffd
Samet <samet.okumus@codbt.com>
2022-10-22 00:59:46 +0300

    first

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', function () { return view('login'); });
+Route::get('/login', function () { return view('login'); });
+Route::get('/dashboard', function () { return view('dashboard'); });
