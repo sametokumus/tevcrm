3b86e06f11894f1a6eb7dd510303c78e69d3a494
Samet <samet.okumus@codbt.com>
2023-02-11 10:50:51 +0300

    sale

--- routes/api_admin.php
+++ routes/api_admin.php
@@ -188,6 +188,7 @@ Route::middleware(['auth:sanctum', 'type.admin'])->group(function (){
 
     //News Feed
     Route::get('newsFeed/getSaleHistoryActions', [NewsFeedController::class, 'getSaleHistoryActions']);
+    Route::get('newsFeed/getTopRequestedProducts', [NewsFeedController::class, 'getTopRequestedProducts']);
 
 });
 
