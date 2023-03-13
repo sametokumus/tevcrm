961b6c0ae15d9acaa21b7c34d5fd5018f0191239
Samet <samet.okumus@codbt.com>
2023-03-13 16:35:33 +0300

    saleDetail

--- app/Http/Controllers/Api/Admin/SaleController.php
+++ app/Http/Controllers/Api/Admin/SaleController.php
@@ -44,7 +44,7 @@ class SaleController extends Controller
             foreach ($sales as $sale) {
                 $offer_request = OfferRequest::query()->where('request_id', $sale->request_id)->where('active', 1)->first();
                 $offer_request['product_count'] = OfferRequestProduct::query()->where('request_id', $offer_request->request_id)->where('active', 1)->count();
-                $offer_request['authorized_personnel'] = Admin::query()->where('id', $offer_request->authorized_personnel_id)->where('active', 1)->first();
+                $offer_request['authorized_personnel'] = Admin::query()->where('id', $offer_request->authorized_personnel_id)->first();
                 $offer_request['company'] = Company::query()->where('id', $offer_request->company_id)->where('active', 1)->first();
                 $offer_request['company_employee'] = Employee::query()->where('id', $offer_request->company_employee_id)->where('active', 1)->first();
                 $sale['request'] = $offer_request;
