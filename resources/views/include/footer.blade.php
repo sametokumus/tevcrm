99cf51c4d0defbacf8063371da61fc7fc1689fea
Samet <samet.okumus@codbt.com>
2023-04-10 01:48:47 +0300

    sale

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -15,7 +15,7 @@
 <script src="js/vendor.min.js"></script>
 <script src="js/app.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha512-jGsMH83oKe9asCpkOVkBnUrDDTp8wl+adkB2D+//JtlxO4SrLoJdhbOysIFQJloQFD+C4Fl1rMsQZF76JjV0eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
 <script src="vendor/popper.min.js"></script>
 
