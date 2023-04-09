e02c7ecef5ac4c40c5493755f75ca08876885cc1
Samet <samet.okumus@codbt.com>
2023-04-10 01:53:39 +0300

    sale

--- app/Http/Controllers/Api/Admin/SaleController.php
+++ app/Http/Controllers/Api/Admin/SaleController.php
@@ -117,11 +117,13 @@ class SaleController extends Controller
 
                 $current_time = Carbon::now();
                 if ($sale->updated_at != null){
-                    $updated_at = Carbon::parse($sale->updated_at);
-                    $updated_at = $updated_at->subHours(3);
+                    $updated_at = $sale->updated_at;
+//                    $updated_at = Carbon::parse($sale->updated_at);
+//                    $updated_at = $updated_at->subHours(3);
                 }else{
-                    $updated_at = Carbon::parse($sale->created_at);
-                    $updated_at = $updated_at->subHours(3);
+                    $updated_at = $sale->created_at;
+//                    $updated_at = Carbon::parse($sale->created_at);
+//                    $updated_at = $updated_at->subHours(3);
                 }
 
                 $difference = $updated_at->diffForHumans($current_time);
