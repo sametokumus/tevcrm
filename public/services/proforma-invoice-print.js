6651fe4979e3e4de91dbd2b27afe6d75860d8f12
Samet <samet.okumus@codbt.com>
2023-04-12 02:40:00 +0300

    sale

--- public/services/proforma-invoice-print.js
+++ public/services/proforma-invoice-print.js
@@ -69,19 +69,19 @@ async function initContact(contact_id, sale_id){
     $('#quote-print .logo-header .date').text(Lang.get("strings.Date") +': '+ today);
     // $('#quote-print .logo-header .offer-id').append(sale_id);
 
+
     $('#quote-print .contact-col address').text('');
 
     $('#quote-print .contact-col address').append('<strong>'+ contact.name +'</strong><br>');
     if (contact.registration_no != '') {
-        $('#quote-print .contact-col address').append('<b>' + Lang.get("strings.Registration No") + ' :</b> ' + contact.registration_no + '<br>');
+        $('#quote-print .contact-col address').append('<b>' + Lang.get("strings.Registration No") + ' :</b> ' + contact.registration_no + '&nbsp;&nbsp;&nbsp;');
     }
     if (contact.registration_office != '') {
-        $('#purchasing-order-print .contact-col address').append('<b>' + Lang.get("strings.Registration Office") + ' :</b> ' + contact.registration_office + '<br>');
+        $('#quote-print .contact-col address').append('<b>' + Lang.get("strings.Registration Office") + ' :</b> ' + contact.registration_office + '<br>');
     }
     $('#quote-print .contact-col address').append('<b>'+ Lang.get("strings.Address") +'</b><br>'+ contact.address +'<br>');
     $('#quote-print .contact-col address').append('<b>'+ Lang.get("strings.Phone") +':</b> '+ contact.phone +'<br>');
     $('#quote-print .contact-col address').append('<b>'+ Lang.get("strings.Email") +':</b> '+ contact.email +'');
-
 }
 
 async function initSale(sale_id){
