303bfd1ec940b9cd624c2a8c4329b94dab2281b6
Samet <samet.okumus@codbt.com>
2023-05-25 03:48:04 +0300

    sale

--- app/Http/Controllers/Api/Admin/SaleController.php
+++ app/Http/Controllers/Api/Admin/SaleController.php
@@ -1244,7 +1244,7 @@ class SaleController extends Controller
 
 //            $sale_offers = SaleOffer::query()->where('sale_id', $sale->sale_id)->where('active', 1)->get();
             $sale_offers = SaleOffer::query()
-                ->leftJoin('packing_list_products', 'packing_list_products.sale_offer_id', '=', 'sale_offers.id')
+                ->join('packing_list_products', 'packing_list_products.sale_offer_id', '=', 'sale_offers.id')
                 ->selectRaw('sale_offers.*')
                 ->where('sale_offers.sale_id', $sale->sale_id)
                 ->where('sale_offers.active', 1)
