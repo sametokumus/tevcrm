6e3e688fcaa9cb8bb35dd6342b6cdad78fc3d1d0
Samet <samet.okumus@codbt.com>
2023-07-15 02:40:44 +0300

    new feed fix

--- app/Http/Controllers/Api/Admin/NewsFeedController.php
+++ app/Http/Controllers/Api/Admin/NewsFeedController.php
@@ -32,7 +32,10 @@ class NewsFeedController extends Controller
                 $last_status = StatusHistory::query()->where('id', $action->id)->first();
                 $last_status['status_name'] = Status::query()->where('id', $last_status->status_id)->first()->name;
                 $admin = User::query()->where('id', $last_status->user_id)->first();
-                $last_status['user_name'] = $admin->name." ".$admin->surname;
+                $last_status['user_name'] = '';
+                if ($admin->name != null && $admin->surname != null) {
+                    $last_status['user_name'] = $admin->name . " " . $admin->surname;
+                }
                 $sale = Sale::query()->where('sale_id', $action->sale_id)->first();
                 $customer = Company::query()->where('id', $sale->customer_id)->first();
                 $sale['customer_name'] = $customer->name;
