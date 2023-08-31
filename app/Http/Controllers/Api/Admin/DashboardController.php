72147aaccd5d211f1d014357eb3219b87219ff67
Samet <samet.okumus@codbt.com>
2023-08-31 07:07:22 +0300

    update currencies

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -1031,24 +1031,31 @@ class DashboardController extends Controller
 //                ->whereYear('sales.created_at', '=', $currentYear)
 //                ->groupBy(DB::raw('DATE_FORMAT(sales.created_at, "%Y-%m-%d")'))
 //                ->toSql();
-            $daysInMonth = Carbon::create($currentYear, $currentMonth)->daysInMonth;
 
-            $dailyTotalApprovedSales = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "%Y-%m-%d") as date FROM (SELECT CURDATE() - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as date FROM (SELECT 0 as a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3) as a CROSS JOIN (SELECT 0 as a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3) as b CROSS JOIN (SELECT 0 as a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3) as c) days'))
-                ->leftJoinSub(
-                    Sale::query()
-                        ->selectRaw('DATE_FORMAT(sales.created_at, "%Y-%m-%d") as date, SUM(sales.grand_total) as total')
-                        ->where('sales.active', 1)
-                        ->whereIn('statuses.period', ['approved'])
-                        ->whereMonth('sales.created_at', '=', $currentMonth)
-                        ->whereYear('sales.created_at', '=', $currentYear)
-                        ->groupBy(DB::raw('DATE_FORMAT(sales.created_at, "%Y-%m-%d")')),
-                    'sales_data',
-                    'all_days.date',
-                    '=',
-                    'sales_data.date'
-                )
-                ->select('all_days.date', DB::raw('COALESCE(sales_data.total, 0) as total'))
-                ->toSql();
+            $firstDayOfMonth = Carbon::create($currentYear, $currentMonth, 1)->startOfDay();
+            $lastDayOfMonth = Carbon::create($currentYear, $currentMonth, 1)->lastOfMonth()->endOfDay();
+
+            $dailyTotalSales = [];
+            $allDays = [];
+
+            for ($date = $firstDayOfMonth; $date <= $lastDayOfMonth; $date->addDay()) {
+                $allDays[$date->toDateString()] = 0;
+            }
+
+            $salesData = Sale::query()
+                ->selectRaw('DATE_FORMAT(created_at, "%Y-%m-%d") as date, SUM(grand_total) as total')
+                ->where('active', 1)
+                ->whereIn('statuses.period', ['approved'])
+                ->whereMonth('created_at', $currentMonth)
+                ->whereYear('created_at', $currentYear)
+                ->groupBy(DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d")'))
+                ->get();
+
+            foreach ($salesData as $sale) {
+                $dailyTotalSales[$sale->date] = $sale->total;
+            }
+
+            $dailyTotalApprovedSales = array_merge($allDays, $dailyTotalSales);
             $approved['daily_sales'] = $dailyTotalApprovedSales;
 
 
