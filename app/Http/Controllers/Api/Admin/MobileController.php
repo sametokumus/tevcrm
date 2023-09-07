43138303e400171e3c4b54ad1083de594155f4f9
Samet <samet.okumus@codbt.com>
2023-09-07 15:47:07 +0300

    update currencies

--- app/Http/Controllers/Api/Admin/MobileController.php
+++ app/Http/Controllers/Api/Admin/MobileController.php
@@ -127,80 +127,96 @@ class MobileController extends Controller
                     ->where('id', $sale_id)
                     ->first();
 
-                $item = array();
-                $offer_request = OfferRequest::query()->where('request_id', $sale->request_id)->first();
-                $company = Company::query()->where('id', $offer_request->company_id)->first();
-                $status = Status::query()->where('id', $sale->status_id)->first();
+                if ($sale) {
 
-                $item['company_id'] = $company->id;
-                $item['company'] = $company->name;
-                $item['completed'] = 0;
-                if ($status->mobile_id == 41) {
-                    $item['completed'] = 1;
-                }
-                $item['confirmation_no'] = 'SMY-' . $sale_id;
-                $item['creation_date'] = Carbon::parse($sale->created_at)->format('d.m.Y h:i:s');
-                $item['currency'] = $sale->currency;
-                $item['order_date'] = Carbon::parse($sale->created_at)->format('Y-m-d');
-                $item['order_id'] = $sale->id;
-                $item['pieces'] = SaleOffer::query()->where('sale_id', $sale->sale_id)->where('active', 1)->sum('offer_quantity');
-                $item['po_no'] = $sale->id;
-                $item['pr_no'] = $sale->id;
-                $item['price_total'] = $sale->grand_total;
-                $item['ref_no'] = $sale->id;
-                $item['subject'] = "";
-                $item['user_id'] = "";
-                $item['customer_po_no'] = "";
+                    $item = array();
+                    $offer_request = OfferRequest::query()->where('request_id', $sale->request_id)->first();
+                    $company = Company::query()->where('id', $offer_request->company_id)->first();
+                    $status = Status::query()->where('id', $sale->status_id)->first();
 
-                $order_items = array();
-                $sale_offers = SaleOffer::query()->where('sale_id', $sale->sale_id)->where('active', 1)->get();
-                $i = 1;
-                foreach ($sale_offers as $sale_offer) {
-                    $product = Product::query()->where('id', $sale_offer->product_id)->first();
-                    $offer_product = OfferProduct::query()->where('id', $sale_offer->offer_product_id)->first();
-                    $measurement = Measurement::query()->where('id', $offer_product->measurement_id)->first();
+                    $item['company_id'] = $company->id;
+                    $item['company'] = $company->name;
+                    $item['completed'] = 0;
+                    if ($status->mobile_id == 41) {
+                        $item['completed'] = 1;
+                    }
+                    $item['confirmation_no'] = 'SMY-' . $sale_id;
+                    $item['creation_date'] = Carbon::parse($sale->created_at)->format('d.m.Y h:i:s');
+                    $item['currency'] = $sale->currency;
+                    $item['order_date'] = Carbon::parse($sale->created_at)->format('Y-m-d');
+                    $item['order_id'] = $sale->id;
+                    $item['pieces'] = SaleOffer::query()->where('sale_id', $sale->sale_id)->where('active', 1)->sum('offer_quantity');
+                    $item['po_no'] = $sale->id;
+                    $item['pr_no'] = $sale->id;
+                    $item['price_total'] = $sale->grand_total;
+                    $item['ref_no'] = $sale->id;
+                    $item['subject'] = "";
+                    $item['user_id'] = "";
+                    $item['customer_po_no'] = "";
+                    $item['success'] = true;
 
-                    $order_item = array();
-                    $order_item['auto_date'] = Carbon::parse($sale_offer->created_at)->format('d.m.Y h:i:s');
-                    $order_item['delivery_day'] = $sale_offer->offer_lead_time;
-                    $order_item['description'] = $product->product_name;
-                    $order_item['order_number'] = $i;
-                    $order_item['progress'] = 0;
-                    $order_item['state'] = $status->mobile_id;
-                    $order_item['total_price'] = $sale_offer->sale_price;
-                    $order_item['unit'] = $sale_offer->offer_quantity;
-                    $order_item['unit_price'] = number_format(($sale_offer->sale_price / $sale_offer->offer_quantity), 2, ".", "");
-                    $order_item['unit_type'] = $measurement->name_tr;
+                    $order_items = array();
+                    $sale_offers = SaleOffer::query()->where('sale_id', $sale->sale_id)->where('active', 1)->get();
+                    $i = 1;
+                    foreach ($sale_offers as $sale_offer) {
+                        $product = Product::query()->where('id', $sale_offer->product_id)->first();
+                        $offer_product = OfferProduct::query()->where('id', $sale_offer->offer_product_id)->first();
+                        $measurement = Measurement::query()->where('id', $offer_product->measurement_id)->first();
 
-                    if ($product->category_id != null){
-                        $category1 = Category::query()->where('id', $product->category_id)->first();
-                        if ($category1->parent_id != 0){
+                        $order_item = array();
+                        $order_item['auto_date'] = Carbon::parse($sale_offer->created_at)->format('d.m.Y h:i:s');
+                        $order_item['delivery_day'] = $sale_offer->offer_lead_time;
+                        $order_item['description'] = $product->product_name;
+                        $order_item['order_number'] = $i;
+                        $order_item['progress'] = 0;
+                        $order_item['state'] = $status->mobile_id;
+                        $order_item['total_price'] = $sale_offer->sale_price;
+                        $order_item['unit'] = $sale_offer->offer_quantity;
+                        $order_item['unit_price'] = number_format(($sale_offer->sale_price / $sale_offer->offer_quantity), 2, ".", "");
+                        $order_item['unit_type'] = $measurement->name_tr;
 
-                            $category2 = Category::query()->where('id', $category1->parent_id)->first();
-                            if ($category2->parent_id != 0){
+                        if ($product->category_id != null) {
+                            $category1 = Category::query()->where('id', $product->category_id)->first();
+                            if ($category1->parent_id != 0) {
 
-                                $category3 = Category::query()->where('id', $category2->parent_id)->first();
-                                $order_item['item_name'] = $category3->name;
+                                $category2 = Category::query()->where('id', $category1->parent_id)->first();
+                                if ($category2->parent_id != 0) {
 
-                            }else{
-                                $order_item['item_name'] = $category2->name;
+                                    $category3 = Category::query()->where('id', $category2->parent_id)->first();
+                                    $order_item['item_name'] = $category3->name;
+
+                                } else {
+                                    $order_item['item_name'] = $category2->name;
+                                }
+
+                            } else {
+                                $order_item['item_name'] = $category1->name;
                             }
 
-                        }else{
-                            $order_item['item_name'] = $category1->name;
+                        } else {
+                            $order_item['item_name'] = "";
                         }
 
-                    }else{
-                        $order_item['item_name'] = "";
+                        array_push($order_items, $order_item);
+                        $i++;
                     }
 
-                    array_push($order_items, $order_item);
-                    $i++;
+                    $item['order_items'] = $order_items;
+
+                    array_push($data, $item);
+
+                }else{
+
+
+                    $item = array();
+                    $item['success'] = false;
+                    $item['err_message'] = 'Sipariş Bulunamadı.';
+
+                    array_push($data, $item);
+
                 }
 
-                $item['order_items'] = $order_items;
 
-                array_push($data, $item);
 
             }
 
