e6dd841500c775ad73124076d8af662889c1709a
Samet <samet.okumus@codbt.com>
2023-09-16 01:40:28 +0300

    reverse pdf test

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -56,8 +56,6 @@
 <script src="js/demo/highlightjs.demo.js" type="fbc7b3cca511f0e3491682e1-text/javascript"></script>
 <script src="js/demo/form-plugins.demo.js" type="fbc7b3cca511f0e3491682e1-text/javascript"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 
 <script>
     $('.summernote').summernote({
