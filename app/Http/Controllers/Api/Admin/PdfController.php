2ef4c89e265cc4e0157c6f0602778ba651ff829d
Samet <samet.okumus@codbt.com>
2023-09-17 21:31:40 +0300

    new fpdf

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -365,7 +365,7 @@ class PdfController extends Controller
 //                $pdf->Ln();
 //            }
 
-            // Set table header
+// Set table header
             $pdf->SetFont('ChakraPetch-Bold', '', 10);
             $pdf->Cell(10, 10, 'N#', 1, 0, 'C');
             $pdf->Cell(20, 10, iconv('utf-8', 'iso-8859-9', __('Ref. Code')), 1, 0, 'C');
@@ -382,15 +382,21 @@ class PdfController extends Controller
                 $pdf->Cell(10, 10, $sale_offer->sequence, 1, 0, 'C');
                 $pdf->Cell(20, 10, iconv('utf-8', 'iso-8859-9', $sale_offer->product_ref_code), 1, 0, 'C');
 
-                // Use MultiCell for product name with a maximum width of 50
-                $pdf->MultiCell(50, 10, iconv('utf-8', 'iso-8859-9', $sale_offer->product_name), 1, 'L');
+                // Handle long product names
+                if (strlen($sale_offer->product_name) > 50) {
+                    $product_name = substr($sale_offer->product_name, 0, 47) . '...'; // Truncate if too long
+                } else {
+                    $product_name = $sale_offer->product_name;
+                }
 
+                $pdf->Cell(50, 10, iconv('utf-8', 'iso-8859-9', $product_name), 1, 0, 'L');  // Use Cell for product name
                 $pdf->Cell(19, 10, iconv('utf-8', 'iso-8859-9', __('Qty')), 1, 0, 'C');
                 $pdf->Cell(16, 10, iconv('utf-8', 'iso-8859-9', __('Unit')), 1, 0, 'C');
                 $pdf->Cell(25, 10, iconv('utf-8', 'iso-8859-9', __('Unit Price')), 1, 0, 'C');
                 $pdf->Cell(30, 10, iconv('utf-8', 'iso-8859-9', __('Total Price')), 1, 0, 'C');
                 $pdf->Cell(20, 10, iconv('utf-8', 'iso-8859-9', __('Lead Time')), 1, 1, 'C');  // Move to the next line
             }
+
 
 
 
