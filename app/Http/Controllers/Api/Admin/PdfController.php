693af759ea8abd7717895966e2b8c7084641ab76
Samet <samet.okumus@codbt.com>
2023-09-18 12:22:23 +0300

    new fpdf

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -618,13 +618,13 @@ class PdfController extends Controller
 
             //TARİH - KOD
 
-            $x = $pageWidth - $pdf->GetStringWidth(__('Date').': '.$document_date) - 13;
             $pdf->SetFont('ChakraPetch-Bold', '', 10);
+            $x = $pageWidth - $pdf->GetStringWidth(__('Date').': '.$document_date) - 10;
             $pdf->SetXY($x, $actual_height + 25);
             $pdf->Cell(0, 0, iconv('utf-8', 'iso-8859-9', __('Date').': '.$document_date), '0', '0', '');
 
-            $x = $pageWidth - $pdf->GetStringWidth($contact->short_code.'-OFR-'.$sale->id) - 16;
             $pdf->SetFont('ChakraPetch-Bold', '', 11);
+            $x = $pageWidth - $pdf->GetStringWidth($contact->short_code.'-OFR-'.$sale->id) - 10;
             $pdf->SetXY($x, $actual_height + 32);
             $pdf->Cell(0, 0, iconv('utf-8', 'iso-8859-9', $contact->short_code.'-OFR-'.$sale->id), '0', '0', '');
 
