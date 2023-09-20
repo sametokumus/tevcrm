26ff26e0a22d751ddbe4f11e59ca0bbfa91a0182
Samet <samet.okumus@codbt.com>
2023-09-20 11:43:43 +0300

    accounting-detail

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -878,7 +878,7 @@ class PdfController extends Controller
                 $row_height = 15;
                 $name_width = $pdf->GetStringWidth($sale_offer->product_name);
                 if ($name_width > 50){
-                    $row_height = 15 / (int)(($name_width / 50) + 1);
+                    $row_height = 15 / (((int)($name_width / 50)) + 1);
                 }
 
                 $pdf->setX(10);
