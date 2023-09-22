1a50af814d47f093ec003af1968cbe00fa3bbfdf
Samet <samet.okumus@codbt.com>
2023-09-22 23:47:40 +0300

    accounting-detail

--- resources/views/accounting-detail.blade.php
+++ resources/views/accounting-detail.blade.php
@@ -187,7 +187,7 @@ $(".timepicker").timepicker({
 
 </div>
 
-    <div class="modal modal-cover fade" id="addPaymentModal">
+    <div class="modal modal-cover fade" id="addPaymentModal" onclick="return false;">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
