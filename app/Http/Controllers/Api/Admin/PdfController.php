66ba68bc66dec7df4d2ef9214f7baf73a309f0c8
Samet <samet.okumus@codbt.com>
2023-09-26 01:37:11 +0300

    accounting-detail

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -628,6 +628,11 @@ class PdfController extends Controller
             $pdf->SetXY($x, $actual_height + 32);
             $pdf->Cell(0, 0, iconv('utf-8', 'iso-8859-9', $contact->short_code.'-OFR-'.$sale->id), '0', '0', '');
 
+            $pdf->SetFont('ChakraPetch-Bold', '', 10);
+            $x = $pageWidth - $pdf->GetStringWidth(__('Expiry Date').': '.$document_date) - 10;
+            $pdf->SetXY($x, $actual_height + 25);
+            $pdf->Cell(0, 0, iconv('utf-8', 'iso-8859-9', __('Expiry Date').': '.$document_date), '0', '0', '');
+
 
             //COMPANY INFO
 
