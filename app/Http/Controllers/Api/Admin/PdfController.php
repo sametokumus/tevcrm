2ecb5eeef997914931228330496460a9272a77f6
Samet <samet.okumus@codbt.com>
2023-09-27 01:12:06 +0300

    accounting-detail

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -747,7 +747,7 @@ class PdfController extends Controller
             $address = iconv('utf-8', 'iso-8859-9', $company->address);
 //            $pdf->Cell(0, 0, $address, '0', '0', '');
             $address_width = $pdf->GetStringWidth($address);
-            $address_height = (((int)($address_width / 100)) + 1) * 3;
+            $address_height = (((int)($address_width / 100)) + 1) * 2;
             $pdf->MultiCell(100, $address_height, $address, 0, 'L');
 
             //QUOTES
