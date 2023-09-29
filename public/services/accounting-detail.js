616e39d0b01b0b0bd2da1f7ddd6c97cde60ded6b
Samet <samet.okumus@codbt.com>
2023-09-29 14:38:58 +0300

    payment term updates

--- public/services/accounting-detail.js
+++ public/services/accounting-detail.js
@@ -231,10 +231,7 @@ async function openAddPaymentModal(packing_list_id, price, currency){
 
     let sale_id = getPathVariable('accounting-detail');
     let data = await serviceGetAccountingPaymentType(sale_id);
-    console.log(data)
-    let term = data.object.term;
-    console.log(term.length)
-    console.log(term)
+    let term = data.term;
     if (term.length != 0){
         document.getElementById('add_payment_payment_type').value = term.payment_type_id;
         addPaymentPaymentTermWithButton(term.expiry);
--- public/services/service.js
+++ public/services/service.js
@@ -2187,7 +2187,7 @@ async function serviceGetPaymentMethods() {
 async function serviceGetAccountingPaymentType(sale_id) {
     const data = await fetchDataGet('/admin/accounting/getAccountingPaymentType/' + sale_id, 'application/json');
     if (data.status == "success") {
-        return data;
+        return data.object;
     } else {
         showAlert('İstek Başarısız.');
     }
