f219710a56046ee0bcca018ebe04b3fe55e4774c
Samet <samet.okumus@codbt.com>
2023-10-04 02:57:30 +0300

    accounting-dashboard

--- app/Http/Controllers/Api/Admin/SaleController.php
+++ app/Http/Controllers/Api/Admin/SaleController.php
@@ -1192,26 +1192,26 @@ class SaleController extends Controller
                 $total_price = $sale->grand_total_with_shipping;
             }
             $payed_price = 0;
-            $transaction = SaleTransaction::query()->where('sale_id', $sale_id)->where('active', 1)->first();
+            $transactions = SaleTransaction::query()->where('sale_id', $sale_id)->where('active', 1)->get();
 
             $sale['remaining_message'] = false;
 
-            if ($transaction) {
-                $payments = SaleTransactionPayment::query()
-                    ->where('sale_transaction_payments.transaction_id', $transaction->transaction_id)
-                    ->where('sale_transaction_payments.active', 1)
-                    ->where('sale_transaction_payments.payment_status_id', 2)
-                    ->get();
+            if ($transactions) {
+                foreach ($transactions as $transaction) {
+                    $payments = SaleTransactionPayment::query()
+                        ->where('sale_transaction_payments.transaction_id', $transaction->transaction_id)
+                        ->where('sale_transaction_payments.active', 1)
+                        ->where('sale_transaction_payments.payment_status_id', 2)
+                        ->get();
 
-                if ($payments) {
-                    foreach ($payments as $payment) {
-                        $payed_price += $payment->payment_price;
+                    if ($payments) {
+                        foreach ($payments as $payment) {
+                            $payed_price += $payment->payment_price;
+                        }
+                        $sale['remaining_message'] = true;
                     }
-                    $sale['remaining_message'] = true;
                 }
             }
-
-
 
             $remaining_price = $total_price - $payed_price;
             $sale['total_price'] = $total_price;
