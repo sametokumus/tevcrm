94b90f2e9bfae520e5c5979e1432167081b72c09
Samet <samet.okumus@codbt.com>
2023-10-05 00:07:02 +0300

    accounting-dashboard

--- public/services/accounting-dashboard.js
+++ public/services/accounting-dashboard.js
@@ -264,7 +264,20 @@ async function getCashFlows(){
                     cssClass: 'apexcharts-xaxis-label',
                 }
             }
-        },
+        },tooltip: {
+            custom: function({ series, seriesIndex, dataPointIndex, w }) {
+                return (
+                    '<div class="tooltip-custom">' +
+                    '<span>X: ' +
+                    w.globals.series[seriesIndex][dataPointIndex].x +
+                    '</span>' +
+                    '<span>Y: ' +
+                    w.globals.series[seriesIndex][dataPointIndex].y +
+                    '</span>' +
+                    '</div>'
+                );
+            }
+        }
     };
 
     var chart = new ApexCharts(document.querySelector("#chart-cashflow"), options);
