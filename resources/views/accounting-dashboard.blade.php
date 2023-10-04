a95889bc018398bc7f0257fa86058274dd3e643f
Samet <samet.okumus@codbt.com>
2023-10-04 16:30:29 +0300

    accounting-dashboard

--- resources/views/accounting-dashboard.blade.php
+++ resources/views/accounting-dashboard.blade.php
@@ -218,9 +218,8 @@ $extra_js='
                             <div class="card mb-3">
 
                                 <div class="card-header d-flex align-items-center bg-white bg-opacity-15">
-                                    <span class="flex-grow-1 fw-400">To Do (5)</span>
-                                    <a href="#" class="text-white text-opacity-50 text-decoration-none me-3"><i class="fa fa-fw fa-redo"></i></a>
-                                    <a href="#" class="text-white text-opacity-50 text-decoration-none"><i class="fa fa-fw fa-trash"></i></a>
+                                    <span class="flex-grow-1 fw-400">10/2023</span>
+                                    <span class="text-white text-opacity-50 text-decoration-none me-3">12.300,00 TRY / 500,00 EUR / 550,00 USD</span>
                                 </div>
 
 
@@ -228,114 +227,17 @@ $extra_js='
 
                                     <div class="list-group-item d-flex px-3">
                                         <div class="me-3 pt-1">
-                                            <i class="far fa-question-circle text-white text-opacity-50 fa-fw fa-lg"></i>
+                                            <i class="far fa-google-wallet text-white text-opacity-50 fa-fw fa-lg"></i>
                                         </div>
                                         <div class="flex-fill">
-                                            <div class="fw-400">Enable open search</div>
-                                            <div class="small text-white text-opacity-50 mb-2">#29949 opened yesterday by Terry</div>
+                                            <div class="fw-400">Müşteri</div>
+                                            <div class="text-white">Ödeme Tutarı</div>
+                                            <div class="small text-white text-opacity-50 mb-2">Satış Kurları</div>
                                             <div>
-                                                <span class="badge border border-gray-300 text-white text-opacity-50">docs</span>
-                                                <span class="badge border border-theme text-theme">feature</span>
-                                            </div>
-                                            <hr class="my-3">
-                                            <div class="d-flex align-items-center mb-2">
-                                                <div class="fw-400 me-2">
-                                                    Task (2/3)
-                                                </div>
-                                                <div>
-                                                    <a href="#" class="text-white text-opacity-50" data-bs-toggle="collapse" data-bs-target="#todoBoard">
-                                                        <i class="fa fa-plus-circle"></i>
-                                                    </a>
-                                                </div>
-                                                <div class="progress progress-xs w-100px me-2 ms-auto" style="height: 6px;">
-                                                    <div class="progress-bar progress-bar-striped bg-theme" style="width: 66%;"></div>
-                                                </div>
-                                                <div class="fs-12px">66%</div>
-                                            </div>
-                                            <div class="form-group mb-0 small">
-                                                <div class="collapse show" id="todoBoard">
-                                                    <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" id="customCheck1" checked="">
-                                                        <label class="form-check-label" for="customCheck1">create ui for autocomplete</label>
-                                                    </div>
-                                                    <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" id="customCheck2" checked="">
-                                                        <label class="form-check-label" for="customCheck2">integrate jquery autocomplete with ui</label>
-                                                    </div>
-                                                    <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" id="customCheck3">
-                                                        <label class="form-check-label" for="customCheck3">backend search return as json data</label>
-                                                    </div>
-                                                </div>
+                                                <span class="badge border border-danger text-danger">gecikmede</span>
                                             </div>
                                         </div>
                                     </div>
-
-
-                                    <a href="#" class="list-group-item list-group-item-action d-flex ps-3">
-                                        <div class="me-3 pt-1">
-                                            <i class="far fa-question-circle text-white text-opacity-50 fa-fw fa-lg"></i>
-                                        </div>
-                                        <div class="flex-fill">
-                                            <div class="fw-400">Investigate adding markdownlint</div>
-                                            <div class="small text-white text-opacity-50 mb-2">#29919 opened 9 days ago by xMediaKron</div>
-                                            <div class="mb-1">
-                                                <span class="badge border border-gray-300 text-white text-opacity-50">build</span>
-                                                <span class="badge border border-indigo text-indigo">v5</span>
-                                            </div>
-                                        </div>
-                                    </a>
-
-
-                                    <a href="#" class="list-group-item list-group-item-action d-flex ps-3">
-                                        <div class="me-3 pt-1">
-                                            <i class="far fa-question-circle text-white text-opacity-50 fa-fw fa-lg"></i>
-                                        </div>
-                                        <div class="flex-fill">
-                                            <div class="fw-400">Add a "Submit a Resource" form</div>
-                                            <div class="small text-white text-opacity-50 mb-2">#29916 opened 9 days ago by Wasbbok</div>
-                                            <div class="mb-1 d-flex align-items-center">
-                                                <div class="me-2"><span class="badge border border-theme text-theme">enhancement</span></div>
-                                                <div class="widget-user-list">
-                                                    <div class="widget-user-list-item w-30px h-30px"><div class="widget-user-list-link w-30px h-30px"><img src="assets/img/user/user-1.jpg" alt=""></div></div>
-                                                    <div class="widget-user-list-item w-30px h-30px ms-n2"><div class="widget-user-list-link w-30px h-30px"><img src="assets/img/user/user-2.jpg" alt=""></div></div>
-                                                    <div class="widget-user-list-item w-30px h-30px ms-n2"><div class="widget-user-list-link w-30px h-30px"><img src="assets/img/user/user-3.jpg" alt=""></div></div>
-                                                    <div class="widget-user-list-item w-30px h-30px ms-n2"><div class="widget-user-list-link w-30px h-30px"><img src="assets/img/user/user-4.jpg" alt=""></div></div>
-                                                    <div class="widget-user-list-item w-30px h-30px ms-n2"><div class="widget-user-list-link w-30px h-30px"><img src="assets/img/user/user-5.jpg" alt=""></div></div>
-                                                    <div class="widget-user-list-item w-30px h-30px ms-n2"><div class="widget-user-list-link w-30px h-30px bg-gray-200 text-gray-500 fs-12px fw-400">+2</div></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-
-
-                                    <a href="#" class="list-group-item list-group-item-action d-flex ps-3">
-                                        <div class="me-3 pt-1">
-                                            <i class="far fa-question-circle text-white text-opacity-50 fa-fw fa-lg"></i>
-                                        </div>
-                                        <div class="flex-fill">
-                                            <div class="fw-400">Custom control border color missing on focus</div>
-                                            <div class="small text-white text-opacity-50 mb-2">#29796 opened 29 days ago by mdo</div>
-                                            <div class="mb-1">
-                                                <span class="badge border border-pink text-pink">docs</span>
-                                            </div>
-                                        </div>
-                                    </a>
-
-
-                                    <a href="#" class="list-group-item list-group-item-action d-flex ps-3">
-                                        <div class="me-3 pt-1">
-                                            <i class="far fa-question-circle text-white text-opacity-50 fa-fw fa-lg"></i>
-                                        </div>
-                                        <div class="flex-fill">
-                                            <div class="fw-400">New design for corporate page</div>
-                                            <div class="mb-2 small text-white text-opacity-50">#29919 opened 19 days ago by sean</div>
-                                            <div class="mb-1">
-                                                <span class="badge border border-gray-300 text-white text-opacity-50">design</span>
-                                                <span class="badge border border-theme text-theme">v5</span>
-                                            </div>
-                                        </div>
-                                    </a>
 
                                 </div>
 
