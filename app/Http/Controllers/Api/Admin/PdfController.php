99fb8269733dedac4b8fc5e8600eecbedefa7c1e
Samet <samet.okumus@codbt.com>
2023-10-25 02:54:36 +0300

    quote pdf optimize

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -3695,86 +3695,42 @@ class PdfController extends Controller
 
                 $currency = $product->currency;
 
-                if (App::getLocale() == 'tr'){
+                if ($lang == 'tr'){
                     $measurement_name = $product->measurement_name_tr;
                 }else{
                     $measurement_name = $product->measurement_name_en;
                 }
 
-                if ($product->lead_time != '' && $product->lead_time != null){
-                    if ($product->lead_time == 1) {
-                        $lead_time = __('Stock');
-                    } elseif (intval($product->lead_time) % 7 == 0) {
-                        $lead_time = (intval($product->lead_time) / 7) . ' ' . __('Week');
-                    } else {
-                        $lead_time = $product->lead_time . ' ' . __('Day');
-                    }
-                }else{
-                    $lead_time = '';
-                }
-
-                $row_height = 15;
                 $pdf->SetFont('ChakraPetch-Regular', '', 9);
 
-                $cleanInput = preg_replace('/[^\p{L}\p{N}\s]/u', ' ', $product->product_name);
-                $inputString = mb_convert_encoding($cleanInput, 'UTF-8', 'auto');
-                $inputString = preg_replace('/[^\x20-\x7E]/u', '', $inputString);
-                $product_name = iconv('utf-8', 'iso-8859-9', $inputString);
+                $x = 40;
+                $pdf->SetXY($x, $y);
 
+                $product_name = $this->textConvert($product->product_name);
                 $name_width = $pdf->GetStringWidth($product_name);
-                if ($name_width > 48){
-                    $wd = (($name_width / 48));
-                    if ($name_width > 60){
-                        $wd = (($name_width / 60));
-                    }
-                    if ($name_width > 100){
-                        $wd = (($name_width / 50));
-                    }
-                    if ($name_width > 110){
-                        $wd = (($name_width / 45));
-                    }
-                    if ($name_width > 200){
-                        $wd = (($name_width / 40));
-                    }
-                    if ($wd >= 0 && $wd < 1){
-                        $row_height = 15;
-                    }else if ($wd >= 1 && $wd < 2){
-                        $row_height = 7.5;
-                    }else if ($wd >= 2 && $wd < 3){
-                        $row_height = 5;
-                    }else if ($wd >= 3 && $wd < 4){
-                        $row_height = 3.75;
-                    }else if ($wd >= 4 && $wd < 5){
-                        $row_height = 3;
-                    }else if ($wd >= 5){
-                        $row_height = 2.5;
-                    }
-
+                $lines_needed = ceil($name_width / 50);
+                $line_height = 8;
+                if ($lines_needed > 1){
+                    $line_height = 5;
                 }
-
-                $pdf->setX(10);
-                $pdf->Cell(10, 15, $i, 1, 0, 'C');
-//                $pdf->Cell(10, 14, '', 1, 0, 'C');
-                $pdf->Cell(20, 15, iconv('utf-8', 'iso-8859-9', $product->ref_code), 1, 0, 'C');
-//                $pdf->Cell(20, 14, iconv('utf-8', 'iso-8859-9', $row_height.' - '.$name_width), 1, 0, 'C');
-
-                // Save the current X and Y position
-                $xPos = $pdf->GetX();
-                $yPos = $pdf->GetY();
+                $row_height = $lines_needed * $line_height;
+                $pdf->MultiCell(50, $line_height, $product_name, 1, 'L');
 
 
-                // Use MultiCell for product name with a width of 50mm
-                $pdf->MultiCell(50, $row_height, $product_name, 'T', 'L');
+                $x = 10;
+                $pdf->SetXY($x, $y);
+                $pdf->Cell(10, $row_height, $i, 1, 0, 'C');
+                $pdf->Cell(20, $row_height, iconv('utf-8', 'iso-8859-9', $product->product_ref_code), 1, 0, 'C');
 
-                // Reset X and move Y to the saved position (next line)
-                $pdf->SetXY($xPos+50, $yPos);
+                $x = 90;
+                $pdf->SetXY($x, $y);
+                $pdf->Cell(19, $row_height, iconv('utf-8', 'iso-8859-9', $product->offer_quantity), 1, 0, 'C');
+                $pdf->Cell(16, $row_height, iconv('utf-8', 'iso-8859-9', $measurement_name), 1, 0, 'C');
+                $pdf->Cell(25, $row_height, iconv('utf-8', 'iso-8859-9', number_format($product->pcs_price, 2,",",".").' '.$product->currency), 1, 0, 'C');
+                $pdf->Cell(30, $row_height, iconv('utf-8', 'iso-8859-9', number_format($product->total_price, 2,",",".").' '.$product->currency), 1, 0, 'C');
+                $pdf->Cell(20, $row_height, iconv('utf-8', 'iso-8859-9', $this->leadtime($product->offer_lead_time)), 1, 1, 'C');
 
-                // Output remaining cells for the current row
-                $pdf->Cell(19, 15, iconv('utf-8', 'iso-8859-9', $product->quantity), 1, 0, 'C');
-                $pdf->Cell(16, 15, iconv('utf-8', 'iso-8859-9', $measurement_name), 1, 0, 'C');
-                $pdf->Cell(25, 15, iconv('utf-8', 'iso-8859-9', number_format($product->pcs_price, 2,",",".").' '.$product->currency), 1, 0, 'C');
-                $pdf->Cell(30, 15, iconv('utf-8', 'iso-8859-9', number_format($product->total_price, 2,",",".").' '.$product->currency), 1, 0, 'C');
-                $pdf->Cell(20, 15, iconv('utf-8', 'iso-8859-9', $lead_time), 1, 1, 'C');  // Move to the next line
+                $y += $row_height;
 
                 $i++;
             }
