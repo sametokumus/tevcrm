6a761434e920c7759c3b604bbed6136c6f9d5163
Samet <samet.okumus@codbt.com>
2023-11-06 04:07:50 +0300

    sale expense

--- app/Http/Controllers/Api/Admin/SaleController.php
+++ app/Http/Controllers/Api/Admin/SaleController.php
@@ -425,12 +425,13 @@ class SaleController extends Controller
                 $request_product = OfferRequestProduct::query()->where('id', $offer_product->request_product_id)->first();
                 $sale_offer['sequence'] = $request_product->sequence;
 
-                if ($sale_offer->offer_price != 0) {
-                    $profit_rate = 100 * (float)$sale_offer->sale_price / $sale_offer->offer_price;
-                }else{
-                    $profit_rate = 0;
-                }
-                $sale_offer['profit_rate'] = '%'.number_format($profit_rate, 2,",","");
+//                if ($sale_offer->offer_price != 0) {
+//                    $profit_rate = 100 * (float)$sale_offer->sale_price / $sale_offer->offer_price;
+//                }else{
+//                    $profit_rate = 0;
+//                }
+//                $sale_offer['profit_rate'] = '%'.number_format($profit_rate, 2,",","");
+                $sale_offer['profit_rate'] = $sale_offer->sale_price;
 
             }
             $sale['sale_offers'] = $sale_offers;
