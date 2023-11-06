cdf4c0b280eafba0cfd81a01835d11e142486946
Samet <samet.okumus@codbt.com>
2023-11-06 05:03:47 +0300

    sale expense

--- resources/views/offer-rev.blade.php
+++ resources/views/offer-rev.blade.php
@@ -26,6 +26,40 @@ $extra_js='
                 </div>
             </div>
 
+            <div class="row d-none">
+
+                <div class="col-md-12">
+                    <div class="card border-theme mb-3">
+                        <div class="card-body p-3">
+                            <table id="offer-request-products" class="table table-bordered text-nowrap key-buttons border-bottom w-100">
+                                <thead>
+                                <tr>
+                                    <th class="border-bottom-0">N#</th>
+                                    <th class="border-bottom-0">ID</th>
+                                    <th class="border-bottom-0">Satın Alma Notu</th>
+                                    <th class="border-bottom-0">Ref. Code</th>
+                                    <th class="border-bottom-0">Ürün Adı</th>
+                                    <th class="border-bottom-0">Miktar</th>
+                                    <th class="border-bottom-0">Birim</th>
+                                    <th class="border-bottom-0 d-none">Product ID</th>
+                                </tr>
+                                </thead>
+                                <tbody id="offer-request-products-body">
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-arrow">
+                            <div class="card-arrow-top-left"></div>
+                            <div class="card-arrow-top-right"></div>
+                            <div class="card-arrow-bottom-left"></div>
+                            <div class="card-arrow-bottom-right"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
             <div class="row">
                 <div class="col-md-12">
                     <h1 class="page-header">
