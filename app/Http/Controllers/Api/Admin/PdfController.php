f638631b9507ef28663f0cb9e242e471012b9901
Samet <samet.okumus@codbt.com>
2023-11-09 16:35:07 +0300

    sale expense

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -49,6 +49,8 @@ class PdfController extends Controller
 
     }
     private function textConvert($text){
+        $text = str_replace('⁰', '0', $text);
+        $text = str_replace('”', '"', $text);
         return iconv('UTF-8', 'ISO-8859-9//TRANSLIT', $text);
         $text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 
