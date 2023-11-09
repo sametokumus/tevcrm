c4b34a57df40e9c44f874b75d7dd0fd5bdcb767e
Samet <samet.okumus@codbt.com>
2023-11-09 03:16:41 +0300

    sale expense

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -219,7 +219,8 @@ class PdfController extends Controller
     }
     private function convertPrice($price, $source, $target, $sale_id){
         $sale = Sale::query()->where('sale_id', $sale_id)->first();
-        $price = number_format($price, 2,",","");
+//        $price = number_format($price, 2,".","");
+        return floatval($price);
         $source = strtolower($source);
         $target = strtolower($target);
         if($source == 'try'){
