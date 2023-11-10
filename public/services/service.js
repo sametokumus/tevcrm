cb5c38cbd10ef9db2e52001edbac7aa0776012e0
Samet <samet.okumus@codbt.com>
2023-11-10 14:17:05 +0300

    sale expense

--- public/services/service.js
+++ public/services/service.js
@@ -240,7 +240,9 @@ function formatTime(time) {
 }
 
 function formatDateAndTime(date, slicer) {
+    console.log(date)
 	date = new Date(date);
+    console.log(date)
 
 	var day = ('0' + date.getDate()).slice(-2);
 	var month = ('0' + (date.getMonth() + 1)).slice(-2);
