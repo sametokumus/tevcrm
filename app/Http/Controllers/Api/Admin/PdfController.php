d3f29bd851528c1559de75882d2635721eceebd3
Samet <samet.okumus@codbt.com>
2023-11-21 02:57:16 +0300

    activity check

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -50,8 +50,8 @@ class PdfController extends Controller
 
     }
     private function textConvert($text){
-//        $inputString = preg_replace('/[^\p{L}\p{N}\s]/u', ' ', $text);
-        $inputString = mb_convert_encoding($text, 'UTF-8', 'auto');
+        $inputString = preg_replace('/[^\p{L}\p{N}\s]/u', ' ', $text);
+        $inputString = mb_convert_encoding($inputString, 'UTF-8', 'auto');
 //        $inputString = preg_replace('/[^\x20-\x7E]/u', '', $inputString);
         return iconv('utf-8', 'iso-8859-9', $inputString);
     }
