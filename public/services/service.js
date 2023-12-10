6047490580c02774639995cefdd1eafee230915e
Samet <samet.okumus@codbt.com>
2023-12-11 02:35:30 +0300

    chat 1

--- public/services/service.js
+++ public/services/service.js
@@ -117,7 +117,7 @@ function xhrDataPost (apiURL, body, callBackFunction) {
 	var returnData;
 	xhr.timeout = 5000;
 	xhr.addEventListener("readystatechange", function() {
-        console.log(this.readyState)
+        console.log(this)
 		if(this.readyState === 4) {
 			callBackFunction(this);
 		}
