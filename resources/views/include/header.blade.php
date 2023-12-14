a1992a10ffafcef36fc9f3e7bcd2ed241b235298
Samet <samet.okumus@codbt.com>
2023-12-14 05:26:54 +0300

    notify

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -103,7 +103,7 @@ $extra_js="";
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end me-lg-3 fs-11px mt-1">
-                    <a class="dropdown-item d-flex align-items-center" href="/my-account">Profilim <i
+                    <a class="dropdown-item d-flex align-items-center" href="/my-dashboard">Profilim <i
                             class="bi bi-people ms-auto text-theme fs-16px my-n1"></i></a>
 {{--                    <a class="dropdown-item d-flex align-items-center" href="#">INBOX <i--}}
 {{--                            class="bi bi-envelope ms-auto text-theme fs-16px my-n1"></i></a>--}}
