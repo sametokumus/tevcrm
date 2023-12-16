cc7418646c6a5c07c7a3e425793373e822f7efb9
Samet <samet.okumus@codbt.com>
2023-12-17 00:34:18 +0300

    notify

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,14 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'verify_peer' => false
+            'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'from' => [
