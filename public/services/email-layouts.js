369c49b833f6a71f2a03c6e7a6725e5fe987c040
Samet <samet.okumus@codbt.com>
2023-12-17 21:30:45 +0300

    offer update

--- public/services/email-layouts.js
+++ public/services/email-layouts.js
@@ -3,16 +3,14 @@
 
 	$(document).ready(function() {
         $(":input").inputmask();
-        $("#add_target_target").maskMoney({thousands:'.', decimal:','});
-        $("#update_target_target").maskMoney({thousands:'.', decimal:','});
 
-        $('#add_notify_form').submit(function (e){
+        $('#add_layout_form').submit(function (e){
             e.preventDefault();
-            addNotifySetting();
+            addLayout();
         });
-        $('#update_notify_form').submit(function (e){
+        $('#update_layout_form').submit(function (e){
             e.preventDefault();
-            updateNotifySetting();
+            updateLayout();
         });
 
 	});
@@ -22,9 +20,6 @@
 		checkLogin();
 		checkRole();
 
-        getStatusesAddSelectId('add_notify_status_id');
-        getAdminRolesAddSelectId('add_notify_role_id');
-        getAdminsAddSelectId('add_notify_staff_id');
         initEmailLayouts();
 
 	});
@@ -47,8 +42,8 @@ async function initEmailLayouts(){
 
         let actions = "";
         if (true){
-            actions = '<button type="button" class="btn btn-outline-secondary btn-sm" onclick="openUpdateEmailLayoutModal(\''+ setting.id +'\');">Düzenle</button>\n' +
-                '      <button type="button" class="btn btn-outline-secondary btn-sm" onclick="deleteEmailLayout(\''+ setting.id +'\');">Sil</button>\n';
+            actions = '<button type="button" class="btn btn-outline-secondary btn-sm" onclick="openUpdateEmailLayoutModal(\''+ layout.id +'\');">Düzenle</button>\n' +
+                '      <button type="button" class="btn btn-outline-secondary btn-sm" onclick="deleteEmailLayout(\''+ layout.id +'\');">Sil</button>\n';
         }
 
         let item = '<tr>\n' +
@@ -74,108 +69,70 @@ async function initEmailLayouts(){
 	});
 }
 
-async function addNotifySetting(){
-    let to_notification = 0;
-    let to_mail = 0;
-    if(document.getElementById('add_notify_to_notification').checked){
-        to_notification = 1;
-    }
-    if(document.getElementById('add_notify_to_mail').checked){
-        to_mail = 1;
-    }
+async function addLayout(){
 
-    let status_id = document.getElementById('add_notify_status_id').value;
-    let role_id = document.getElementById('add_notify_role_id').value;
-
-    let receivers = [];
-    let receiver_objs = $('#add_notify_staff_id').find(':selected');
-    for (let i = 0; i < receiver_objs.length; i++) {
-        receivers.push(receiver_objs[i].value);
-    }
+    let name = document.getElementById('add_layout_name').value;
+    let subject = document.getElementById('add_layout_subject').value;
+    let text = $('#add_layout_text').summernote('code');
 
 
     let formData = JSON.stringify({
-        "status_id": status_id,
-        "role_id": role_id,
-        "receivers": receivers,
-        "to_notification": to_notification,
-        "to_mail": to_mail
+        "name": name,
+        "subject": subject,
+        "text": text
     });
 
     console.log(formData);
 
-    let returned = await servicePostAddNotificationSetting(formData);
+    let returned = await servicePostAddEmailLayout(formData);
     if (returned){
-        $("#add_notify_form").trigger("reset");
-        $('.select2-selection__rendered li').remove();
-        initNotifySettings();
+        $("#add_layout_form").trigger("reset");
+        initEmailLayouts();
     }else{
         alert("Hata Oluştu");
     }
 }
 
 
-async function openUpdateNotifySettingModal(setting_id){
-    await getStatusesAddSelectId('update_notify_status_id');
-    await getAdminRolesAddSelectId('update_notify_role_id');
-    await getAdminsAddSelectId('update_notify_staff_id');
-    $("#updateNotifySettingModal").modal('show');
-    initUpdateNotifySettingModal(setting_id)
+async function openUpdateLayoutModal(layout_id){
+    $("#updateLayoutModal").modal('show');
+    initUpdateLayoutModal(layout_id)
 }
-async function initUpdateNotifySettingModal(setting_id){
-    document.getElementById('update_notify_form').reset();
-    let data = await serviceGetNotifySettingById(setting_id);
-    let setting = data.setting;
-    document.getElementById('update_notify_id').value = setting.id;
-    document.getElementById('update_notify_status_id').value = setting.status_id;
-    document.getElementById('update_notify_role_id').value = setting.role_id;
-    let values = JSON.parse(setting.receivers);
-    $("#update_notify_staff_id").val(values).trigger("change");
-    if (setting.is_notification == 1){ document.getElementById('update_notify_to_notification').checked = true; }
-    if (setting.is_mail == 1){ document.getElementById('update_notify_to_mail').checked = true; }
+async function initUpdateLayoutModal(layout_id){
+    document.getElementById('update_layout_form').reset();
+    let data = await serviceGetNotifySettingById(layout_id);
+    let layout = data.layout;
+    document.getElementById('update_layout_id').value = layout.id;
+    document.getElementById('update_layout_name').value = layout.name;
+    document.getElementById('update_layout_subject').value = layout.subject;
+    document.getElementById('update_layout_text').value = layout.text;
 }
-async function updateNotifySetting(){
-    let id = document.getElementById('update_notify_id').value;
-    let to_notification = 0;
-    let to_mail = 0;
-    if(document.getElementById('update_notify_to_notification').checked){
-        to_notification = 1;
-    }
-    if(document.getElementById('update_notify_to_mail').checked){
-        to_mail = 1;
-    }
-
-    let status_id = document.getElementById('update_notify_status_id').value;
-    let role_id = document.getElementById('update_notify_role_id').value;
-
-    let receivers = [];
-    let receiver_objs = $('#update_notify_staff_id').find(':selected');
-    for (let i = 0; i < receiver_objs.length; i++) {
-        receivers.push(receiver_objs[i].value);
-    }
+async function updateLayout(){
+    let id = document.getElementById('update_layout_id').value;
+    let name = document.getElementById('update_layout_name').value;
+    let subject = document.getElementById('update_layout_subject').value;
+    let text = $('#update_layout_text').summernote('code');
 
 
     let formData = JSON.stringify({
         "id": id,
-        "status_id": status_id,
-        "role_id": role_id,
-        "receivers": receivers,
-        "to_notification": to_notification,
-        "to_mail": to_mail
+        "name": name,
+        "subject": subject,
+        "text": text
     });
-    let returned = await servicePostUpdateNotificationSetting(formData);
+    let returned = await servicePostUpdateEmailLayout(formData);
     if (returned){
-        $("#update_notify_form").trigger("reset");
-        $("#updateNotifySettingModal").modal('hide');
-        initNotifySettings();
+        $("#update_layout_form").trigger("reset");
+        $("#updateLayoutModal").modal('hide');
+        initEmailLayouts();
     }else{
-        alert('Güncelleme yapılırken bir hata oluştu. Lütfen tekrar deneyiniz!');
+        showAlert('Güncelleme yapılırken bir hata oluştu. Lütfen tekrar deneyiniz!');
     }
 }
 
-async function deleteNotifySetting(setting_id){
-    let returned = await serviceGetDeleteNotificationSetting(setting_id);
+async function deleteEmailLayout(layout_id){
+    let returned = await serviceGetDeleteEmailLayout(layout_id);
     if(returned){
-        initNotifySettings();
+        initEmailLayouts();
     }
 }
--- resources/views/email-layouts.blade.php
+++ resources/views/email-layouts.blade.php
@@ -26,33 +26,16 @@ $extra_js='
                         <div class="card border-theme mb-3">
                             <div class="card-body">
                                 <div class="row p-3">
-                                    <div class="col-md-2 mb-3">
-                                        <label class="form-label">Sipariş Durumu</label>
-                                        <select class="form-control" id="add_notify_status_id" required>
-
-                                        </select>
+                                    <div class="col-md-12 mb-3">
+                                        <label class="form-label">Şablon Adı</label>
+                                        <input type="text" class="form-control" id="add_layout_name">
                                     </div>
-                                    <div class="col-md-2 mb-3">
-                                        <label class="form-label">Yetki</label>
-                                        <select class="form-control" id="add_notify_role_id">
-
-                                        </select>
+                                    <div class="col-md-12 mb-3">
+                                        <label class="form-label">Konu</label>
+                                        <input type="text" class="form-control" id="add_layout_subject">
                                     </div>
-                                    <div class="col-md-5 mb-3">
-                                        <label class="form-label">Personel</label>
-                                        <select name="add_notify_staff_id[]" class="form-control form-select select2" multiple="multiple" id="add_notify_staff_id">
-                                        </select>
-                                    </div>
-                                    <div class="col-md-3 mb-3">
-                                        <label class="form-label d-block">Uyarı Türü Seçiniz</label>
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="checkbox" value="" id="add_notify_to_notification" />
-                                            <label class="form-check-label" for="add_notify_to_notification">Bildirim</label>
-                                        </div>
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="checkbox" value="" id="add_notify_to_mail" />
-                                            <label class="form-check-label" for="add_notify_to_mail">Mail</label>
-                                        </div>
+                                    <div class="col-md-12 mb-3">
+                                        <textarea name="text" class="summernote" id="add_layout_text" title="Contents"></textarea>
                                     </div>
                                     <div class="col-md-12 mb-3">
                                         <button type="submit" class="btn btn-theme w-100">Kaydet</button>
@@ -98,44 +81,27 @@ $extra_js='
     </div>
 </div>
 <!--app-content close-->
-<div class="modal modal-cover fade" id="updateNotifySettingModal">
+<div class="modal modal-cover fade" id="updateLayoutModal">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Hedef Güncelle</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
-            <form method="post" action="#" id="update_notify_form">
+            <form method="post" action="#" id="update_layout_form">
                 <div class="modal-body">
                     <div class="row mb-4">
                         <div class="col-md-12 mb-3">
-                            <input type="hidden" value="" class="form-control" id="update_notify_id" />
-                            <label class="form-label">Sipariş Durumu</label>
-                            <select class="form-control" id="update_notify_status_id" required>
-
-                            </select>
+                            <input type="hidden" value="" class="form-control" id="update_layout_id" />
+                            <label class="form-label">Şablon Adı</label>
+                            <input type="text" class="form-control" id="update_layout_name">
                         </div>
                         <div class="col-md-12 mb-3">
-                            <label class="form-label">Yetki</label>
-                            <select class="form-control" id="update_notify_role_id">
-
-                            </select>
+                            <label class="form-label">Konu</label>
+                            <input type="text" class="form-control" id="update_layout_subject">
                         </div>
                         <div class="col-md-12 mb-3">
-                            <label class="form-label">Personel</label>
-                            <select name="update_notify_staff_id[]" class="form-control form-select select2" multiple="multiple" id="update_notify_staff_id">
-                            </select>
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label class="form-label d-block">Uyarı Türü Seçiniz</label>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" value="" id="update_notify_to_notification" />
-                                <label class="form-check-label" for="update_notify_to_notification">Bildirim</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" value="" id="update_notify_to_mail" />
-                                <label class="form-check-label" for="update_notify_to_mail">Mail</label>
-                            </div>
+                            <textarea name="text" class="summernote" id="update_layout_text" title="Contents"></textarea>
                         </div>
                     </div>
                 </div>
