a68c4f9a011b0d11f38ea1e10cfe30ccdd9db660
Samet <samet.okumus@codbt.com>
2023-12-23 02:50:13 +0300

    offer update

--- public/services/my-dashboard.js
+++ public/services/my-dashboard.js
@@ -840,6 +840,7 @@ function filterCompanies() {
     for (let i = 0; i < companyItems.length; i++) {
         let companyName = companyItems[i].querySelector('.company-name').textContent.toLowerCase();
         console.log(companyName)
+        console.log(companyName.includes(searchText))
         // Check if the company name contains the search text
         if (companyName.includes(searchText)) {
             companyItems[i].style.display = 'flex';  // Show the company item
