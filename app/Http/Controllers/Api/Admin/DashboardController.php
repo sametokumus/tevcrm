4e2e50a3ed0eb2fffc1faac2a4f3c879f1c1dd14
Samet <samet.okumus@codbt.com>
2023-12-29 23:15:27 +0300

    offer update

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -300,10 +300,10 @@ class DashboardController extends Controller
                 array_push($previousYearArray, $month_array2);
             }
 
-//            return response(['message' => __('İşlem Başarılı.'), 'status' => 'success', 'object' => [
-//                'sales' => $currentYearArray,
-//                'previous_sales' => $previousYearArray
-//            ]]);
+            return response(['message' => __('İşlem Başarılı.'), 'status' => 'success', 'object' => [
+                'sales' => $currentYearArray,
+                'previous_sales' => $previousYearArray
+            ]]);
 
             $sales = array();
             $previous_sales = array();
