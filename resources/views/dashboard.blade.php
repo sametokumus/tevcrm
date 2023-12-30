95cd3a77c904f6bc5a34f14dd49253ffd12f8833
Samet <samet.okumus@codbt.com>
2023-12-31 01:19:45 +0300

    offer update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -247,7 +247,7 @@ $extra_js='
 
                     <div class="flex-fill">
                         <div class="d-flex fw-bold small mb-3">
-                            <span class="flex-grow-1">TOPLAM TALEP (Bu Ay - Bu Yıl)</span>
+                            <span class="flex-grow-1">TOPLAM TALEP <small>(Bu Ay - Yıl)</small></span>
                             <a href="#" data-toggle="card-expand"
                                class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
                         </div>
@@ -530,7 +530,7 @@ $extra_js='
 
                     <div class="flex-fill">
                         <div class="d-flex fw-bold small mb-3">
-                            <span class="flex-grow-1">TOPLAM SİPARİŞ (Bu Ay - Bu Yıl)</span>
+                            <span class="flex-grow-1">TOPLAM SİPARİŞ <small>(Bu Ay - Yıl)</small></span>
                             <a href="#" data-toggle="card-expand"
                                class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
                         </div>
@@ -568,7 +568,7 @@ $extra_js='
 
                     <div class="flex-fill">
                         <div class="d-flex fw-bold small mb-3">
-                            <span class="flex-grow-1">TOPLAM GÖRÜŞME <small>(Bu Ay - Bu Yıl)</small></span>
+                            <span class="flex-grow-1">TOPLAM GÖRÜŞME <small>(Bu Ay - Yıl)</small></span>
                             <a href="#" data-toggle="card-expand"
                                class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
                         </div>
