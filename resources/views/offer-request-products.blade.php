a1701f9dc1926d4752ed0fa4893472f32434e771
Samet <samet.okumus@codbt.com>
2024-01-06 15:48:35 +0300

    offer update

--- resources/views/offer-request-products.blade.php
+++ resources/views/offer-request-products.blade.php
@@ -98,7 +98,7 @@ $extra_js='
                     <div class="col-md-12">
                         <div class="card border-theme mb-3">
                             <div class="card-body p-3">
-                                <h6><a href="img/sample-import.xlsx" class="fw-400 text-white-50">Örnek Import dökümanını buradan indirebilirsiniz</a></h6>
+                                <h6><a href="img/sample-import.xlsx" class="fw-400 text-dark-50">Örnek Import dökümanını buradan indirebilirsiniz</a></h6>
                                 <form action="#" method="post" id="import_data_form" enctype="multipart/form-data" style="display: none;">
                                     <input type="file" name="import_file" id="import_file" onchange="$('#import_submit_btn').click();">
                                     <button id="import_submit_btn" type="submit">import</button>
