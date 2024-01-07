be989715f5431616edd4ca79e5188a7f2602926d
Samet <samet.okumus@codbt.com>
2024-01-08 00:50:39 +0300

    offer update

--- resources/views/staff-dashboard.blade.php
+++ resources/views/staff-dashboard.blade.php
@@ -16,20 +16,18 @@ $extra_js='
                 <div class="card-body">
 
                     <div class="d-flex fw-bold small mb-3">
-                        <span class="flex-grow-1">ONAYLANAN SATIŞLAR</span>
+                        <span class="flex-grow-1">YILLIK SATIŞLARIN DAĞILIMI</span>
                         <a href="#" data-toggle="card-expand"
                            class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
+                        <div class="d-block">
+                            Hedef: 2.000.000,00 TRY | Satış: 1.600.000,00 TRY
+                        </div>
                     </div>
 
 
                     <div class="mb-3">
-                        <div id="chart-approved-monthly">
+                        <div id="chart-sales-monthly">
 
-                            <div class="spinners">
-                                <div class="spinner spinner-grow spinner-grow-1 m-1 text-success"></div>
-                                <div class="spinner spinner-grow spinner-grow-2 m-1 text-success"></div>
-                                <div class="spinner spinner-grow spinner-grow-3 m-1 text-success"></div>
-                            </div>
                         </div>
                     </div>
 
