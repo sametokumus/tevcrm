38cada725779d617fad7b78902ab66397c41b143
Samet <samet.okumus@codbt.com>
2024-01-10 13:09:22 +0300

    offer update

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -152,6 +152,12 @@ $extra_js="";
                         <span class="menu-text">Dashboard</span>
                     </a>
                 </div>
+                <div id="nav-staff-dashboard" class="menu-item @if( Request::segment(1)== 'staff-dashboard' ) active @endif">
+                    <a href="/staff-dashboard" class="menu-link">
+                        <span class="menu-icon"><i class="bi bi-people-fill"></i></span>
+                        <span class="menu-text">Personel Profilleri</span>
+                    </a>
+                </div>
 {{--                <div id="nav-news-feed" class="d-none menu-item @if( Request::segment(1)== 'news-feed' ) active @endif">--}}
 {{--                    <a href="/news-feed" class="menu-link">--}}
 {{--                        <span class="menu-icon"><i class="bi bi-columns-gap"></i></span>--}}
