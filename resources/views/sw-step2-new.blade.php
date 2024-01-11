9117eb479fb3efb3f0f2740ad7084e50d16032a9
Samet <samet.okumus@codbt.com>
2024-01-11 23:51:50 +0300

    offer update

--- resources/views/sw-step2-new.blade.php
+++ resources/views/sw-step2-new.blade.php
@@ -171,6 +171,20 @@ $extra_js='
                 <div class="col-md-12">
                     <div class="card border-theme mb-3">
                         <div class="card-body p-3 overflow-auto">
+                            <div class="row justify-content-center">
+                                <div class="col-12">
+                                    <div class="d-block">
+                                        <h6>Lütfen bekleyiniz. Tedarikçi teklifleri oluşturuluyor.</h6>
+                                    </div>
+                                    <div class="d-block">
+                                        <div class="spinners">
+                                            <div class="spinner spinner-grow spinner-grow-1 m-1 text-dark"></div>
+                                            <div class="spinner spinner-grow spinner-grow-2 m-1 text-dark"></div>
+                                            <div class="spinner spinner-grow spinner-grow-3 m-1 text-dark"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <table id="new-offer-detail" class="table table-bordered text-nowrap key-buttons border-bottom">
                                 <thead>
 
