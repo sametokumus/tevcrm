7e4deefcfd94fec78661e547fcf2e00b676889b9
Samet <samet.okumus@codbt.com>
2024-01-22 02:09:43 +0300

    offer update

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -12,13 +12,13 @@
     {{ $slot }}
 
     {{-- Subcopy --}}
-    @isset($subcopy)
-        @slot('subcopy')
-            @component('mail::subcopy')
-                {{ $subcopy }}
-            @endcomponent
-        @endslot
-    @endisset
+{{--    @isset($subcopy)--}}
+{{--        @slot('subcopy')--}}
+{{--            @component('mail::subcopy')--}}
+{{--                {{ $subcopy }}--}}
+{{--            @endcomponent--}}
+{{--        @endslot--}}
+{{--    @endisset--}}
 
     {{-- Footer --}}
     @slot('footer')
