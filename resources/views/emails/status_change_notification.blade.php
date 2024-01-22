d48935d07dcd6cbff8cab6cb9c9de401696e9ca7
Samet <samet.okumus@codbt.com>
2024-01-22 03:44:27 +0300

    offer update

--- resources/views/emails/status_change_notification.blade.php
+++ resources/views/emails/status_change_notification.blade.php
@@ -1,10 +1,4 @@
 @component('mail::message')
-    {{-- Greeting --}}
-    @if (! empty($greeting))
-        # {{ $greeting }}
-    @endif
-
-    {{-- Logo --}}
 
     <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
         <tr>
@@ -14,9 +8,9 @@
                         <td align="center">
                             <table border="0" cellpadding="0" cellspacing="0" role="presentation">
                                 <tr>
-                                    <td>
-                                        <img src="{{ $notify_logo }}" style="width: 150px; margin: auto;" alt="">
-                                        <p>{!! $message !!}</p>
+                                    <td style="text-align: center;">
+                                        <img src="{{ $notify_logo }}" style="width: 150px; margin-bottom: 30px;" alt="">
+                                        <p style="color: #2d3748;">{!! $message !!}</p>
                                         <a href="{{ $actionUrl }}" class="button button-primary" target="_blank" rel="noopener">{{ $actionText }}</a>
                                     </td>
                                 </tr>
@@ -28,30 +22,4 @@
         </tr>
     </table>
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
-
-    {{-- Salutation --}}
-    @if (! empty($salutation))
-        {{ $salutation }}
-    @else
-        {{--        @lang('Regards'),<br>--}}
-        {{ config('app.name') }}
-    @endif
-
-
-    {{-- Subcopy --}}
-    @isset($actionText)
-        @slot('subcopy')
-            @lang(
-                "Eğer \":actionText\" butonuna tıkladığınızda sorun yaşıyorsanız aşağıdaki linki tarayıcınızın adres çubuğuna yapıştırabilirsiniz.\n".
-                'Bağlantı adresi:',
-                [
-                    'actionText' => $actionText,
-                ]
-            ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        @endslot
-    @endisset
 @endcomponent
