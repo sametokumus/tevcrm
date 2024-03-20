51a25aa3ceccbad9f7b70d65850a740746714f9c
Samet <samet.okumus@codbt.com>
2024-03-20 16:42:55 +0300

    test

---
+++ public/services/categories.js
@@ -0,0 +1,74 @@
+(function($) {
+    "use strict";
+
+	$(document).ready(function() {
+	});
+
+    $(window).on('load', function () {
+
+		checkLogin();
+		checkRole();
+        initCategories();
+
+	});
+
+})(window.jQuery);
+
+function checkRole(){
+	return true;
+}
+async function initCategories(){
+	let data = await serviceGetCustomers();
+    $("#category-datatable").dataTable().fnDestroy();
+    $('#category-datatable tbody > tr').remove();
+
+    $.each(data.categories, function (i, category) {
+
+        let item = '<tr>\n' +
+            '                  <td>\n' +
+            '                      <p class="mb-0">'+ checkNull(category.name) +'</p>\n' +
+            '                  </td>\n' +
+            '                  <td>\n' +
+            '                  <div class="btn-list">\n' +
+            '                      <a href="update-category/'+ category.id +'" id="bDel" type="button" class="btn  btn-sm btn-theme">\n' +
+            '                          <span class="bi bi-pencil-square"></span>\n' +
+            '                      </a>\n' +
+            '                      <button id="bEdit" type="button" class="btn btn-sm btn-danger" onclick="deleteCategory(\''+ category.id +'\')">\n' +
+            '                          <span class="bi bi-trash3"></span>\n' +
+            '                      </button>\n' +
+            '                  </div>\n' +
+            '                  </td>\n' +
+            '              </tr>';
+        $('#category-datatable tbody').append(item);
+    });
+
+    $('#category-datatable').DataTable({
+        responsive: true,
+        columnDefs: [
+            { responsivePriority: 1, targets: 0 },
+            { responsivePriority: 2, targets: -1 }
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                text: 'Yeni Kategori',
+                className: 'btn btn-primary',
+                action: function ( e, dt, node, config ) {
+                    window.location = '/add-category';
+                }
+            }
+        ],
+        pageLength : -1,
+        language: {
+            url: "services/Turkish.json"
+        }
+    });
+
+}
+
+async function deleteCategory(category_id){
+    let returned = await serviceGetDeleteCategory(category_id);
+    if(returned){
+        initCategories();
+    }
+}
---
+++ resources/views/categories.blade.php
@@ -0,0 +1,45 @@
+@include('include.header')
+<?php
+$extra_js='
+<script src="services/header-title.js"></script>
+<script src="services/customers.js"></script>
+';
+?>
+
+<main class="main mainheight px-4">
+
+    <!-- content -->
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center mb-2">
+            <div class="col-12">
+                <h6 class="title">Kategoriler</h6>
+
+                <div class="row">
+                    <!-- timesheet -->
+                    <div class="col-12 col-md-12 position-relative">
+                        <div class="card border-0 mb-4">
+                            <div class="card-body p-4">
+                                <table id="category-datatable" class="table table-bordered nowrap key-buttons border-bottom">
+                                    <thead>
+                                    <tr class="text-muted">
+                                        <th class="">Kategori</th>
+                                        <th class="w-50px"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+
+@include('include.footer')
