422f08ad9ada87649c0ee9feae871e5109ca3791
Samet <samet.okumus@codbt.com>
2024-03-21 19:27:05 +0300

    test

---
+++ public/services/update-test.js
@@ -0,0 +1,56 @@
+(function($) {
+    "use strict";
+
+	$(document).ready(function() {
+
+        $(":input").inputmask();
+        $("#update_test_price").maskMoney({thousands:'.', decimal:','});
+
+        $('#update_test_form').submit(function (e){
+            e.preventDefault();
+            updateTest()
+        });
+	});
+
+    $(window).on('load', function () {
+
+		checkLogin();
+		checkRole();
+        getCategoriesAddSelectId('update_test_category');
+        initTest();
+
+	});
+
+})(window.jQuery);
+
+function checkRole(){
+	return true;
+}
+async function initTest(){
+    let test_id = getPathVariable('update-test');
+    let data = await serviceGetTestById(test_id);
+    let test = data.test;
+    document.getElementById('update_test_category').value = test.category_id;
+    document.getElementById('update_test_name').value = test.name;
+    document.getElementById('update_test_sample_count').value = test.sample_count;
+    document.getElementById('update_test_sample_description').value = test.sample_description;
+    document.getElementById('update_test_total_day').value = test.total_day;
+    document.getElementById('update_test_price').value = changePriceToDecimal(test.price);
+}
+async function updateTest(){
+
+    let formData = JSON.stringify({
+        "category_id": document.getElementById('update_test_category').value,
+        "name": document.getElementById('update_test_name').value,
+        "sample_count": document.getElementById('update_test_sample_count').value,
+        "sample_description": document.getElementById('update_test_sample_description').value,
+        "total_day": document.getElementById('update_test_total_day').value,
+        "price": changePriceToDecimal(document.getElementById('update_test_price').value),
+    });
+    console.log(formData);
+
+    let returned = await servicePostAddTest(formData);
+    if (returned){
+        $("#update_test_form").trigger("reset");
+    }
+}
---
+++ resources/views/update-test.blade.php
@@ -0,0 +1,109 @@
+@include('include.header')
+<?php
+$extra_js='
+<script src="services/header-title.js"></script>
+<script src="services/update-test.js"></script>
+';
+?>
+
+<main class="main mainheight px-4">
+
+    <!-- content -->
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center mb-2">
+            <div class="col-12">
+                <h6 class="title">Test Güncelle</h6>
+                <form id="add_test_form">
+                    <div class="row">
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <input type="text" placeholder="Test Adı" id="update_test_name" required class="form-control border-start-0">
+                                        <label>Test Adı</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <select class="form-select border-0" id="update_test_category" required>
+                                            <option value="">Seçiniz...</option>
+                                        </select>
+                                        <label>Kategori</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <input type="number" placeholder="Numune Adedi" id="update_test_sample_count" required class="form-control border-start-0">
+                                        <label>Numune Adedi</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <input type="text" placeholder="Numune Açıklaması" id="update_test_sample_description" class="form-control border-start-0">
+                                        <label>Numune Açıklaması</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <input type="number" placeholder="Test Süresi (Gün)" id="update_test_total_day" required class="form-control border-start-0">
+                                        <label>Test Süresi (Gün)</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-2">
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <input type="text" placeholder="Test Bedeli" id="update_test_price" class="form-control border-start-0">
+                                        <label>Test Bedeli</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                        <div class="col-12 col-md-4 mb-2">
+                            <button type="submit" class="btn btn-theme">Kaydet</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+
+@include('include.footer')
