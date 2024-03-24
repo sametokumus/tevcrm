8411296353a849ef2aaa941637679d7101151501
Samet <samet.okumus@codbt.com>
2024-03-25 01:11:15 +0300

    test

--- app/Http/Controllers/Api/Admin/OfferController.php
+++ app/Http/Controllers/Api/Admin/OfferController.php
@@ -237,10 +237,10 @@ class OfferController extends Controller
                 'test_total' => $test_total_price
             ]);
             OfferDetail::query()->where('id', $offer_detail_id)->update([
-                'active' => 0
+                'active' => 1
             ]);
 
-            return response(['message' => __('Test silme işlemi başarılı.'), 'status' => 'success']);
+            return response(['message' => __('Test silme işlemi başarılı.'), 'status' => 'successs']);
         } catch (ValidationException $validationException) {
             return response(['message' => __('Lütfen girdiğiniz bilgileri kontrol ediniz.'), 'status' => 'validation-001']);
         } catch (QueryException $queryException) {
