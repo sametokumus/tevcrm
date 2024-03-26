67b7ef93bf367c006640a24d7e3ba3cd9ac96d74
Samet <samet.okumus@codbt.com>
2024-03-26 09:43:19 +0300

    test

--- resources/views/add-offer-accountings.blade.php
+++ resources/views/add-offer-accountings.blade.php
@@ -30,13 +30,13 @@ $extra_js='
                         </div>
                         <div class="col-12 col-md-12 mb-2">
                             <div class="form-group mb-3 position-relative check-valid">
-                                <div class="form-check">
+                                <div class="form-check form-check-inline">
                                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                                     <label class="form-check-label" for="flexRadioDefault1">
                                         Default radio
                                     </label>
                                 </div>
-                                <div class="form-check">
+                                <div class="form-check form-check-inline">
                                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
                                     <label class="form-check-label" for="flexRadioDefault2">
                                         Default checked radio
