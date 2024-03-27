51b2a1e3cc0aa9301e0faf8f80063ec508d630a1
Samet <samet.okumus@codbt.com>
2024-03-27 12:41:24 +0300

    test

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -49,14 +49,13 @@ $extra_js='
 <div class="modal modal-cover fade" id="updateStatusModal">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">DURUM GÜNCELLE</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
             <form method="post" action="#" id="update_status_form">
                 <div class="modal-body">
                     <div class="row mb-4">
-                        <div class="col-12 col-md-6 mb-2">
+                        <div class="col-md-12 mb-3">
+                            <h6 class="title">Durum Güncelle</h6>
+                        </div>
+                        <div class="col-12 col-md-12 mb-2">
                             <div class="form-group mb-3 position-relative check-valid">
                                 <div class="input-group input-group-lg">
                                     <span class="input-group-text text-theme bg-white border-end-0"><i
@@ -72,9 +71,14 @@ $extra_js='
                             <div class="invalid-feedback mb-3">Add valid data</div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-theme">Kaydet</button>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-link text-danger" type="button" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-trash h4 me-2"></i> Vazgeç</button>
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-theme" type="submit"><i class="bi bi-send me-2"></i> Kaydet</button>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
@@ -84,19 +88,23 @@ $extra_js='
 <div class="modal modal-cover fade" id="deleteOfferModal">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">TEKLİF SİLME</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
             <div class="modal-body">
                 <form method="post" action="#" id="delete_offer_form">
                     <div class="row mb-4">
                         <div class="col-md-12 mb-3">
+                            <h6 class="title">Teklif Silme</h6>
+                        </div>
+                        <div class="col-md-12 mb-3">
                             <input type="hidden" class="form-control" id="delete_offer_id" required>
                             <label class="form-label">Teklifi silmek istediğinize emin misiniz?</label>
                         </div>
-                        <div class="col-md-12 mb-3">
-                            <button type="submit" class="btn btn-danger">Sil</button>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-link text-danger" type="button" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-trash h4 me-2"></i> Vazgeç</button>
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-theme" type="submit"><i class="bi bi-send me-2"></i> Kaydet</button>
                         </div>
                     </div>
                 </form>
