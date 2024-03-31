011895b4edc4128b078bd63c490183a51133a8ce
Samet <samet.okumus@codbt.com>
2024-03-31 23:32:49 +0300

    test

--- resources/views/offer-detail.blade.php
+++ resources/views/offer-detail.blade.php
@@ -310,6 +310,46 @@ $extra_js='
 
 </main>
 
+<div class="modal modal-cover fade" id="updateStatusModal">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <form method="post" action="#" id="update_status_form">
+                <div class="modal-body">
+                    <div class="row mb-4">
+                        <div class="col-md-12 mb-3">
+                            <h6 class="title">Durum Güncelle</h6>
+                        </div>
+                        <div class="col-12 col-md-12 mb-2">
+                            <input type="hidden" class="form-control" id="update_offer_id" required>
+                            <div class="form-group mb-3 position-relative check-valid">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text text-theme bg-white border-end-0"><i
+                                            class="bi bi-highlighter"></i></span>
+                                    <div class="form-floating">
+                                        <select class="form-select border-0" id="update_offer_status" required>
+                                            <option value="">Seçiniz...</option>
+                                        </select>
+                                        <label>Durum</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="invalid-feedback mb-3">Add valid data</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-link text-danger" type="button" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-trash h4 me-2"></i> Vazgeç</button>
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-theme" type="submit"><i class="bi bi-send me-2"></i> Kaydet</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <div class="modal modal-cover fade" id="addDocumentModal">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
