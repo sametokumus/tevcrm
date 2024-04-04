2f8cf645a99df8f711e6a11fc6bdeb713bf49f65
Samet <samet.okumus@codbt.com>
2024-04-05 02:08:29 +0300

    test

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -587,7 +587,7 @@ class PdfController extends Controller
                     $line_height = 5;
                 }
 
-                $x = 15;
+                $x = 14.5;
                 $pdf->SetXY($x, $pdf->GetY());
                 $old_y = $y;
 
@@ -595,12 +595,12 @@ class PdfController extends Controller
                 $total_y = $pdf->getY() + $row_height;
                 if ($total_y > 270){
                     $pdf->AddPage();
-                    $pdf->SetXY(15, 10);
+                    $pdf->SetXY(14.5, 10);
                     $y = 10;
                     $old_y = $pdf->getY();
                 }
 
-                $pdf->SetXY(15, $old_y);
+                $pdf->SetXY(14.5, $old_y);
                 if ($lines_needed == $lines_needed1){
                     $pdf->MultiCell(48, $line_height, $product_name, 1, 'L');
                 }else{
