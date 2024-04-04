9a1f0391c5b8bed6c62a5c596080c79965265613
Samet <samet.okumus@codbt.com>
2024-04-04 12:32:41 +0300

    test

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -637,7 +637,7 @@ class PdfController extends Controller
 //                $pdf->Cell(24, $row_height, iconv('utf-8', 'iso-8859-9', $sale_offer->offer_price.' '.$currency), 1, 0, 'C');
 //                $pdf->Cell(18, $row_height, iconv('utf-8', 'iso-8859-9', $this->leadtime($sale_offer->offer_lead_time)), 1, 1, 'C');
 
-                $y = $new_y;
+                $y += $row_height;
                 $test_count++;
 
             }
