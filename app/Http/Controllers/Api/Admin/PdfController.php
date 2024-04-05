34075fe0e6cbc278348324f55a9b41a7d33e23ef
Samet <samet.okumus@codbt.com>
2024-04-05 03:28:57 +0300

    test

--- app/Http/Controllers/Api/Admin/PdfController.php
+++ app/Http/Controllers/Api/Admin/PdfController.php
@@ -539,7 +539,7 @@ class PdfController extends Controller
                 if ($offer_detail->product_name != null && $offer_detail->product_name != '') {
                     $product_name = $this->textConvert($offer_detail->product_name);
                     $product_name_width = $pdf->GetStringWidth($product_name);
-                    $lines_needed1 = ceil($product_name_width / 40);
+                    $lines_needed1 = ceil($product_name_width / 47);
                 }else{
                     $lines_needed1 = 1;
                 }
@@ -547,7 +547,7 @@ class PdfController extends Controller
                 if ($offer_detail->name != null && $offer_detail->name != '') {
                     $test_name = $this->textConvert($offer_detail->name);
                     $test_name_width = $pdf->GetStringWidth($test_name);
-                    $lines_needed2 = ceil($test_name_width / 37);
+                    $lines_needed2 = ceil($test_name_width / 44);
                 }else{
                     $lines_needed2 = 1;
                 }
